21c33020976f5edb2c43df5853d9fdffd83c2df4
ronyhourani <ronyhourani@gmail.com>
2023-08-28 09:27:50 +0300

    [Portf/Wine] JavaScript body background color changing

--- ecommerce_wine/ecommerce_wine.js
+++ ecommerce_wine/ecommerce_wine.js
@@ -1,7 +1,22 @@
+// Body background color array
+const bodyBackgroundColorArray = ["#dad3c1", "#808080", "#dab0a4", "#781e1e"];
+let currentColorIndex = 0;
+var newBodyBackgroundColor = bodyBackgroundColorArray[currentColorIndex];
+console.log(newBodyBackgroundColor);
+
+// Getting access to root
+var root = document.querySelector(":root");
+console.log(root);
+var rootStyles = getComputedStyle(root);
+console.log(rootStyles);
+
+// Getting access to button
 const buttons = document.querySelectorAll("[data-carousel-button]");
 
+// Click event
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
+    // Changing slide
     const offset = button.dataset.carouselButton === "next" ? 1 : -1;
 
     const slides = button
@@ -18,5 +33,13 @@ buttons.forEach((button) => {
     slides.children[newIndex].dataset.active = true;
 
     delete activeSlide.dataset.active;
+
+    // changing body background color
+    currentColorIndex =
+      (currentColorIndex + 1) % bodyBackgroundColorArray.length;
+    console.log(currentColorIndex);
+    var newBodyBackgroundColor = bodyBackgroundColorArray[currentColorIndex];
+    root.style.setProperty("--body-background-color", newBodyBackgroundColor);
+    console.log(newBodyBackgroundColor);
   });
 });
