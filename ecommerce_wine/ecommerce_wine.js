3410f660e2efa458dcd5e0f8e38b7e4050e1b39c
ronyhourani <ronyhourani@gmail.com>
2023-09-11 09:22:57 +0300

    [Portf/Wine]Fix the colors

--- ecommerce_wine/ecommerce_wine.js
+++ ecommerce_wine/ecommerce_wine.js
@@ -1,6 +1,6 @@
 var currentIndex = 0;
 // Body background color array
-const bodyBackgroundColorArray = ["#dad3c1", "#c2c0c0", "#966645", "#d11414"];
+const bodyBackgroundColorArray = ["#dad3c1", "#c2c0c0", "#40a19c", "#cf3f3f"];
 var newBodyBackgroundColor = bodyBackgroundColorArray[currentIndex];
 console.log(newBodyBackgroundColor);
 
@@ -8,8 +8,8 @@ console.log(newBodyBackgroundColor);
 const sectionBackgroundColorArray = [
   "#f9f4e5",
   "#D3D3D3",
-  "#ad7650",
-  "#d13636",
+  "#5ae0da",
+  "#d45454",
 ];
 var newSectionBackgroundColor = sectionBackgroundColorArray[currentIndex];
 
@@ -18,7 +18,7 @@ const normalTextColorArray = ["#2b2925", "#000", "#f2edd7", "#fdfafa"];
 var newNormalTextColor = normalTextColorArray[currentIndex];
 
 // Hover background color array
-const hoverBackgroundColorArray = ["#D3D3D3", "#ad7650", "#d13636", "#f9f4e5"];
+const hoverBackgroundColorArray = ["#D3D3D3", "#5ae0da", "#d45454", "#f9f4e5"];
 var newHoverBackgroundColor = hoverBackgroundColorArray[currentIndex];
 
 // Hover text color
@@ -26,11 +26,11 @@ const hoverTextColorArray = ["#000", "#f2edd7", "#fdfafa", "#2b2925"];
 var newHoverTextColor = hoverTextColorArray[currentIndex];
 
 // Border color
-const borderColorArray = ["#dad3c1", "#c2c0c0", "#966645", "#d11414"];
+const borderColorArray = ["#dad3c1", "#c2c0c0", "#40a19c", "#cf3f3f"];
 var newBorderColor = borderColorArray[currentIndex];
 
 // Hover border color
-const hoverBorderColorArray = ["#c2c0c0", "#966645", "#d11414", "#dad3c1"];
+const hoverBorderColorArray = ["#c2c0c0", "#40a19c", "#cf3f3f", "#dad3c1"];
 var newHoverBorderColor = hoverBorderColorArray[currentIndex];
 
 // Getting access to root
