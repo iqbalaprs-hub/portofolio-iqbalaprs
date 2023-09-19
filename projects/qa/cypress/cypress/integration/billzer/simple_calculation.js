7a76385537c9384e95d70d2332617f86077f3316
ronyhourani <ronyhourani@gmail.com>
2023-09-19 18:51:29 +0300

    [Portf/Billzer] simple_calculation ameliorated

--- projects/qa/cypress/cypress/integration/billzer/simple_calculation.js
+++ projects/qa/cypress/cypress/integration/billzer/simple_calculation.js
@@ -2,47 +2,53 @@
 
 describe("My Second Test Suite", function () {
   beforeEach(() => {
+    // Prereq: the user visits the page "https://billzer.com/"
     cy.visit("https://billzer.com/");
-    // Default number of member is 3
-    cy.get(".input-hg").should("have.value", "3");
-    // Type in the Name input: Trip
-    cy.get("#text").type("Trip");
 
-    // Click the button GO
+    /* 
+    Prereq:
+    The user typed in the Main page the following information:
+    Member count input: 3
+    Name input: Trip
+    */
+    cy.get(".input-hg").should("have.value", "3");
+    cy.get("#text").type("Trip");
     cy.get("#go").click();
 
-    // At the top of the page, I see the name "Trip"
-    // There are 3 members forms to fill
-    cy.get("#occasion").contains("Trip");
-    cy.get(".calc").find(".userwrap").should("have.length", 3);
-
-    // Type in Person's name in form 1: Person1
+    /*
+    Prereq:
+    In the calculation page, the user:
+    - Type in Person's name in form 1: Person1
+    - Type in Person's name in form 2: Person2
+    - Type in Person's name in form 3: Person3
+    - Type in expense's name in form1: Ticket
+    - Type in expense's name in form2: Ticket
+    - Type in expense's name in form3: Ticket
+    */
     cy.get(".calc .userwrap").eq(0).find("input.name").type("Person1");
-    // Type in Person's name in form 2: Person2
     cy.get(".calc .userwrap").eq(1).find("input.name").type("Person2");
-    // Type in Person's name in form 3: Person3
     cy.get(".calc .userwrap").eq(2).find("input.name").type("Person3");
-
-    // Type in expense's name in form1: Ticket
     cy.get(".calc .inputwrap").eq(0).find("input.was").type("Ticket");
-    // Type in expense's name in form2: Ticket
     cy.get(".calc .inputwrap").eq(1).find("input.was").type("Ticket");
-    // Type in expense's name in form3: Ticket
     cy.get(".calc .inputwrap").eq(2).find("input.was").type("Ticket");
   });
 
-  it("Edge case: User cannot calculate if nobody paid ", function () {
-    // Type in expense's amount in Person1: 0
+  it("1.Edge case: User cannot calculate if nobody paid ", function () {
+    // 1.1: Type in expense's amount in Person1: 0
     cy.get(".calc .inputwrap").eq(0).find("input.price").type("0");
-    // Type in expense's amount in Person2: 0
+
+    // 1.2: Type in expense's amount in Person2: 0
     cy.get(".calc .inputwrap").eq(1).find("input.price").type("0");
-    // Type in expense's amount in Person3: 0
+
+    // 1.3: Type in expense's amount in Person3: 0
     cy.get(".calc .inputwrap").eq(2).find("input.price").type("0");
 
-    // Click the Button "Calculate & Save!"
+    // 1.4: Click the Button "Calculate & Save!"
+    /*
+    Expected result:
+    A sentence under the title "Who pays whom how much" appears saying: No results to display, your group is even or there is no data
+    */
     cy.get("#submitbutton").click();
-
-    // A sentence under the title "Who pays whom how much" appears saying: No results to display, your group is even or there is no data
     cy.get(".the-return")
       .find(".initialwarning")
       .should(
@@ -51,19 +57,25 @@ describe("My Second Test Suite", function () {
       );
   });
 
-  it("Nominal case: User calculates if only one person paid", function () {
-    // Type in expense's amount in Person1: 100
+  it("2.Nominal case: User calculates if only one person paid", function () {
+    // 2.1: Type in expense's amount in Person1: 100
     cy.get(".calc .inputwrap").eq(0).find("input.price").type("100");
-    // Type in expense's amount in Person2: 0
+
+    // 2.2: Type in expense's amount in Person2: 0
     cy.get(".calc .inputwrap").eq(1).find("input.price").type("0");
-    // Type in expense's amount in Person3: 0
+
+    // 2.3: Type in expense's amount in Person3: 0
     cy.get(".calc .inputwrap").eq(2).find("input.price").type("0");
 
-    // Click the Button "Calculate & Save!"
+    // 2.4: Click the Button "Calculate & Save!"
+    /*
+    Expected result:
+    Calculation is done and a box appears saying:
+      - Person2 owns 33.33 to Person1
+      - Person3 owns 33.33 to Person1
+    */
     cy.get("#submitbutton").click();
 
-    // Calculation is done and a box appears saying:
-    // Person2 owns 33.33 to Person1
     cy.get(".the-return .ergi")
       .eq(0)
       .find(".deb")
@@ -77,7 +89,6 @@ describe("My Second Test Suite", function () {
       .find("div.cred")
       .should("have.text", "Person1");
 
-    // Person3 owns 33.33 to Person1
     cy.get(".the-return .ergi")
       .eq(1)
       .find(".deb")
@@ -92,19 +103,25 @@ describe("My Second Test Suite", function () {
       .should("have.text", "Person1");
   });
 
-  it("Nominal case: User calculates if 3 persons paid and 2 of them paid identically", function () {
-    // Type in expense's amount in Person1: 100
+  it("3.Nominal case: User calculates if 3 persons paid and 2 of them paid identically", function () {
+    // 3.1: Type in expense's amount in Person1: 100
     cy.get(".calc .inputwrap").eq(0).find("input.price").type("100");
-    // Type in expense's amount in Person2: 50
+
+    // 3.2: Type in expense's amount in Person2: 50
     cy.get(".calc .inputwrap").eq(1).find("input.price").type("50");
-    // Type in expense's amount in Person3: 50
+
+    // 3.3: Type in expense's amount in Person3: 50
     cy.get(".calc .inputwrap").eq(2).find("input.price").type("50");
 
-    // Click the Button "Calculate & Save!"
+    // 3.4: Click the Button "Calculate & Save!"
+    /*
+    Expected result:
+    Calculation is done and a box appears saying:
+      - Person2 owns 16.67 to Person1
+      - Person3 owns 16.67 to Person1
+    */
     cy.get("#submitbutton").click();
 
-    // Calculation is done and a box appears saying:
-    // Person2 owns 16.67 to Person1
     cy.get(".the-return .ergi")
       .eq(0)
       .find(".deb")
@@ -118,7 +135,6 @@ describe("My Second Test Suite", function () {
       .find("div.cred")
       .should("have.text", "Person1");
 
-    // Person3 owns 16.67 to Person1
     cy.get(".the-return .ergi")
       .eq(1)
       .find(".deb")
@@ -133,19 +149,25 @@ describe("My Second Test Suite", function () {
       .should("have.text", "Person1");
   });
 
-  it("Nominal case: User calculates if 3 persons paid differently", function () {
-    // Type in expense's amount in Person1: 100
+  it("4.Nominal case: User calculates if 3 persons paid differently", function () {
+    // 4.1: Type in expense's amount in Person1: 100
     cy.get(".calc .inputwrap").eq(0).find("input.price").type("100");
-    // Type in expense's amount in Person2: 50
+
+    // 4.2: Type in expense's amount in Person2: 50
     cy.get(".calc .inputwrap").eq(1).find("input.price").type("50");
-    // Type in expense's amount in Person3: 20
+
+    // 4.3: Type in expense's amount in Person3: 20
     cy.get(".calc .inputwrap").eq(2).find("input.price").type("20");
 
-    // Click the Button "Calculate & Save!"
+    // 4.4: Click the Button "Calculate & Save!"
+    /*
+    Expected result:
+    Calculation is done and a box appears saying:
+      - Person2 owns 6.67 to Person1
+      - Person3 owns 36.67 to Person1
+    */
     cy.get("#submitbutton").click();
 
-    // Calculation is done and a box appears saying:
-    // Person2 owns 6.67 to Person1
     cy.get(".the-return .ergi")
       .eq(0)
       .find(".deb")
@@ -159,7 +181,6 @@ describe("My Second Test Suite", function () {
       .find("div.cred")
       .should("have.text", "Person1");
 
-    // Person3 owns 36.67 to Person1
     cy.get(".the-return .ergi")
       .eq(1)
       .find(".deb")
@@ -174,18 +195,22 @@ describe("My Second Test Suite", function () {
       .should("have.text", "Person1");
   });
 
-  it("Nominal case: User calculates if 3 persons paid identically", function () {
-    // Type in expense's amount in Person1: 100
+  it("5.Nominal case: User calculates if 3 persons paid identically", function () {
+    // 5.1: Type in expense's amount in Person1: 100
     cy.get(".calc .inputwrap").eq(0).find("input.price").type("100");
-    // Type in expense's amount in Person2: 100
+
+    // 5.2: Type in expense's amount in Person2: 100
     cy.get(".calc .inputwrap").eq(1).find("input.price").type("100");
-    // Type in expense's amount in Person3: 100
+
+    // 5.3: Type in expense's amount in Person3: 100
     cy.get(".calc .inputwrap").eq(2).find("input.price").type("100");
 
-    // Click the Button "Calculate & Save!"
+    // 5.4: Click the Button "Calculate & Save!"
+    /*
+    Expected result:
+    A sentence under the title "Who pays whom how much" appears saying: No results to display, your group is even or there is no data"
+    */
     cy.get("#submitbutton").click();
-
-    // A sentence under the title "Who pays whom how much" appears saying: No results to display, your group is even or there is no data
     cy.get(".the-return")
       .find(".initialwarning")
       .should(
@@ -194,19 +219,25 @@ describe("My Second Test Suite", function () {
       );
   });
 
-  it("Nominal case: User calculates if number added are decimals", function () {
-    // Type in expense's amount in Person1: 100
+  it("6.Nominal case: User calculates if number added are decimals", function () {
+    // 6.1: Type in expense's amount in Person1: 100
     cy.get(".calc .inputwrap").eq(0).find("input.price").type("100");
-    // Type in expense's amount in Person2: 50.5
+
+    // 6.2: Type in expense's amount in Person2: 50.5
     cy.get(".calc .inputwrap").eq(1).find("input.price").type("50.5");
-    // Type in expense's amount in Person3: 30.4545
+
+    // 6.3: Type in expense's amount in Person3: 30.4545
     cy.get(".calc .inputwrap").eq(2).find("input.price").type("30.4545");
 
     // Click the Button "Calculate & Save!"
+    /*
+    Expected result:
+    Calculation is done and a box appears saying:
+      - Person2 owns 9.82 to Person1
+      - Person3 owns 29.87 to Person1
+    */
     cy.get("#submitbutton").click();
 
-    // Calculation is done and a box appears saying:
-    // Person2 owns 9.82 to Person1
     cy.get(".the-return .ergi")
       .eq(0)
       .find(".deb")
@@ -220,7 +251,6 @@ describe("My Second Test Suite", function () {
       .find("div.cred")
       .should("have.text", "Person1");
 
-    // Person3 owns 29.87 to Person1
     cy.get(".the-return .ergi")
       .eq(1)
       .find(".deb")
@@ -235,10 +265,11 @@ describe("My Second Test Suite", function () {
       .should("have.text", "Person1");
   });
 
-  it("Edge case: User cannot calculate if a negative number is entered", function () {
+  it.only("7.Edge case: User cannot calculate if a negative number is entered", function () {
     // Type in expense's amount in Person1: -
     cy.get(".calc .inputwrap").eq(0).find("input.price").type("-");
 
+    cy.wait(3000);
     cy.on("window:alert"),
       (str) => {
         expect(str).to.equal(
