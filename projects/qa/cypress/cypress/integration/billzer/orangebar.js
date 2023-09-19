9cdc9b7a4ad085c5045864fbd20ec2752468a85a
ronyhourani <ronyhourani@gmail.com>
2023-09-19 09:48:34 +0300

    [Portf/Billzer] orangebar.js ameliorated

--- projects/qa/cypress/cypress/integration/billzer/orangebar.js
+++ projects/qa/cypress/cypress/integration/billzer/orangebar.js
@@ -2,45 +2,48 @@
 
 describe("My Second Test Suite", function () {
   beforeEach(() => {
+    // Prereq: the user visits the page "https://billzer.com/"
     cy.visit("https://billzer.com/");
-    // Default number of member is 3
-    cy.get(".input-hg").should("have.value", "3");
-    // Type in the Name input: Trip
-    cy.get("#text").type("Trip");
 
-    // Click the button GO
+    /* 
+    Prereq:
+    The user typed in the Main page the following information:
+    Member count input: 3
+    Name input: Trip
+    */
+    cy.get(".input-hg").should("have.value", "3");
+    cy.get("#text").type("Trip");
     cy.get("#go").click();
 
-    // At the top of the page, I see the name "Trip"
-    // There are 3 members forms to fill
-    cy.get("#occasion").contains("Trip");
-    cy.get(".calc").find(".userwrap").should("have.length", 3);
+    /*
+    Prereq:
+    In the calculation page, the user:
+    - Type in Person's name in form 1: Person1
+    - Type in Person's name in form 2: Person2
+    - Type in Person's name in form 3: Person3
+    - Type in expense's name in form1: Ticket
+    - Type in expense's name in form2: Ticket
+    - Type in expense's name in form3: Ticket
+    - Type in expense's amount in Person1: 100
 
-    // Type in Person's name in form 1: Person1
+    NOTE: Both button "Calculate & Save!" and the orange bar "Unsaved changes. Hit the calculate button below or click here" appear once you make any change in any form
+    */
     cy.get(".calc .userwrap").eq(0).find("input.name").type("Person1");
-    // Type in Person's name in form 2: Person2
     cy.get(".calc .userwrap").eq(1).find("input.name").type("Person2");
-    // Type in Person's name in form 3: Person3
     cy.get(".calc .userwrap").eq(2).find("input.name").type("Person3");
-
-    // Type in expense's name in form1: Ticket
     cy.get(".calc .inputwrap").eq(0).find("input.was").type("Ticket");
-    // Type in expense's name in form2: Ticket
     cy.get(".calc .inputwrap").eq(1).find("input.was").type("Ticket");
-    // Type in expense's name in form3: Ticket
     cy.get(".calc .inputwrap").eq(2).find("input.was").type("Ticket");
-
-    // Type in expense's amount in Person1: 100
     cy.get(".calc .inputwrap").eq(0).find("input.price").type("100");
   });
 
-  it("Nominal case: Both blue button (Calculate & Save!) and orange bar appears when changin number", function () {
+  it("1.Nominal case: Both blue button (Calculate & Save!) and orange bar appears when changing number", function () {
     //          --------------------        FIRST CHANGE: CHANGE A NUMBER   -----------------------------------------
 
-    // Button (submitbutton!) appears
+    // 1.1: Expected result: Both blue button and orange bar reappear
+    // Blue button
     cy.get("#submitbutton");
-
-    // Find the orange bar
+    // Orange bar
     cy.get(".savediv2")
       .find(".save2")
       .should(
@@ -49,18 +52,22 @@ describe("My Second Test Suite", function () {
       )
       .click();
 
-    // Click on the orange bar
+    // 1.2: Click on the orange bar
+    //
+    /*
+    Expected result:
+    Alert bar appear saying "Saved. Result is at the bottom.  Use this link to share or edit with your friends:  (link)"  
+    Calculation is done and a box appears saying:
+      - Person2 owns 33.33 to Person1
+      - Person3 owns 33.33 to Person1
+    */
     cy.get(".savediv2").click();
-
-    // Alert bar appear saying "Saved. Result is at the bottom.  Use this link to share or edit with your friends:  (link)"
     cy.on("window:alert", (Text) => {
       expect(Text).to.contains(
         "Saved. Result is at the bottom.  Use this link to share or edit with your friends"
       );
     });
 
-    // Calculation is done and a box appears saying:
-    // Person2 owns 33.33 to Person1
     cy.get(".the-return .ergi")
       .eq(0)
       .find(".deb")
@@ -73,8 +80,6 @@ describe("My Second Test Suite", function () {
       .eq(0)
       .find("div.cred")
       .should("have.text", "Person1");
-
-    // Person3 owns 33.33 to Person1
     cy.get(".the-return .ergi")
       .eq(1)
       .find(".deb")
@@ -89,14 +94,20 @@ describe("My Second Test Suite", function () {
       .should("have.text", "Person1");
   });
 
-  it("Nominal case: Both blue button (Calculate & Save!) and orange bar appears when clicking on button more and minus button", function () {
+  it("2.Nominal case: Both blue button (Calculate & Save!) and orange bar appears when clicking on button more and minus button", function () {
     //          --------------------        SECOND CHANGE: CLICK ON THE BUTTON MORE AND THE BUTTON MINUS   -----------------------------------------
 
-    // Click on Button (submitbutton!)
+    // 2.1: Click on "Calculate & Save!"
+    /*
+    Expected result:
+    Calculation is done and a box appears saying:
+      - Person2 owns 33.33 to Person1
+      - Person3 owns 33.33 to Person1
+
+    Note: We had to do it so that we can show the clicking on "more" will activate the orange bar and the blue button
+    */
     cy.get("#submitbutton").click();
 
-    // Calculation is done and a box appears saying:
-    // Person2 owns 33.33 to Person1
     cy.get(".the-return .ergi")
       .eq(0)
       .find(".deb")
@@ -110,7 +121,6 @@ describe("My Second Test Suite", function () {
       .find("div.cred")
       .should("have.text", "Person1");
 
-    // Person3 owns 33.33 to Person1
     cy.get(".the-return .ergi")
       .eq(1)
       .find(".deb")
@@ -124,13 +134,10 @@ describe("My Second Test Suite", function () {
       .find("div.cred")
       .should("have.text", "Person1");
 
-    // Click on button"more" of Person1
+    // 2.2: Click on button"more" of Person1
+    // Expected result: Both blue button and orange bar reappear
     cy.get(".calc .userwrap").eq(0).find(".pluswrap").click();
-
-    // Button (submitbutton!) appears
     cy.get("#submitbutton");
-
-    // Find the orange bar
     cy.get(".savediv2")
       .find(".save2")
       .should(
@@ -139,18 +146,21 @@ describe("My Second Test Suite", function () {
       )
       .click();
 
-    // Click on the orange bar
+    // 2.3: Click on the orange bar
+    /*
+    Expected result:
+    Alert bar appear saying "Saved. Result is at the bottom.  Use this link to share or edit with your friends:  (link)"
+    Calculation is done and a box appears saying:
+      - Person2 owns 33.33 to Person1
+      - Person3 owns 33.33 to Person1
+    */
     cy.get(".savediv2").click();
-
-    // Alert bar appear saying "Saved. Result is at the bottom.  Use this link to share or edit with your friends:  (link)"
     cy.on("window:alert", (Text) => {
       expect(Text).to.contains(
         "Saved. Result is at the bottom.  Use this link to share or edit with your friends"
       );
     });
 
-    // Calculation is done and a box appears saying:
-    // Person2 owns 33.33 to Person1
     cy.get(".the-return .ergi")
       .eq(0)
       .find(".deb")
@@ -164,7 +174,6 @@ describe("My Second Test Suite", function () {
       .find("div.cred")
       .should("have.text", "Person1");
 
-    // Person3 owns 33.33 to Person1
     cy.get(".the-return .ergi")
       .eq(1)
       .find(".deb")
@@ -178,7 +187,8 @@ describe("My Second Test Suite", function () {
       .find("div.cred")
       .should("have.text", "Person1");
 
-    // Click on minus button to remove the second expense of Person1
+    // 2.4: Click on minus button to remove the second expense of Person1
+    // Expected result: Both blue button and orange bar reappear
     cy.get(".calc .userwrap")
       .eq(0)
       .find(".inputwrap")
@@ -186,10 +196,7 @@ describe("My Second Test Suite", function () {
       .find(".minus")
       .click();
 
-    // Button (submitbutton!) appears
     cy.get("#submitbutton");
-
-    // Find the orange bar
     cy.get(".savediv2")
       .find(".save2")
       .should(
@@ -198,18 +205,21 @@ describe("My Second Test Suite", function () {
       )
       .click();
 
-    // Click on the orange bar
+    // 2.5: Click on the orange bar
+    /*
+    Expected result:
+    Alert bar appear saying "Saved. Result is at the bottom.  Use this link to share or edit with your friends:  (link)"
+    Calculation is done and a box appears saying:
+      - Person2 owns 33.33 to Person1
+      - Person3 owns 33.33 to Person1
+    */
     cy.get(".savediv2").click();
-
-    // Alert bar appear saying "Saved. Result is at the bottom.  Use this link to share or edit with your friends:  (link)"
     cy.on("window:alert", (Text) => {
       expect(Text).to.contains(
         "Saved. Result is at the bottom.  Use this link to share or edit with your friends"
       );
     });
 
-    // Calculation is done and a box appears saying:
-    // Person2 owns 33.33 to Person1
     cy.get(".the-return .ergi")
       .eq(0)
       .find(".deb")
@@ -223,7 +233,6 @@ describe("My Second Test Suite", function () {
       .find("div.cred")
       .should("have.text", "Person1");
 
-    // Person3 owns 33.33 to Person1
     cy.get(".the-return .ergi")
       .eq(1)
       .find(".deb")
@@ -241,11 +250,17 @@ describe("My Second Test Suite", function () {
   it("Nominal case: Both blue button (Calculate & Save!) and orange bar appears when excluding a person from an expense", function () {
     //          --------------------        THIRD CHANGE: EXCLUDE A PERSON   -----------------------------------------
 
-    // Click on Button (submitbutton!)
+    // 3.1: Click on "Calculate & Save!"
+    /*
+    Expected result:
+    Calculation is done and a box appears saying:
+      - Person2 owns 33.33 to Person1
+      - Person3 owns 33.33 to Person1
+
+    Note: We had to do it so that we can show the clicking on "more" will activate the orange bar and the blue button
+    */
     cy.get("#submitbutton").click();
 
-    // Calculation is done and a box appears saying:
-    // Person2 owns 33.33 to Person1
     cy.get(".the-return .ergi")
       .eq(0)
       .find(".deb")
@@ -259,7 +274,6 @@ describe("My Second Test Suite", function () {
       .find("div.cred")
       .should("have.text", "Person1");
 
-    // Person3 owns 33.33 to Person1
     cy.get(".the-return .ergi")
       .eq(1)
       .find(".deb")
@@ -273,7 +287,8 @@ describe("My Second Test Suite", function () {
       .find("div.cred")
       .should("have.text", "Person1");
 
-    // Exclude Person1 from the expense of Person1
+    // 3.2: Exclude Person1 from the expense of Person1
+    // Expected result: Both blue button and orange bar reappear
     cy.get(".calc .userwrap")
       .eq(0)
       .find(".inputwrap")
@@ -289,10 +304,7 @@ describe("My Second Test Suite", function () {
       .eq(0)
       .click();
 
-    // Button (submitbutton!) appears
     cy.get("#submitbutton");
-
-    // Find the orange bar
     cy.get(".savediv2")
       .find(".save2")
       .should(
@@ -301,18 +313,21 @@ describe("My Second Test Suite", function () {
       )
       .click();
 
-    // Click on the orange bar
+    // 3.3: Click on the orange bar
+    /*
+    Expected result:
+    Alert bar appear saying "Saved. Result is at the bottom.  Use this link to share or edit with your friends:  (link)"
+    Calculation is done and a box appears saying:
+      - Person2 owns 50 to Person1
+      - Person3 owns 50 to Person1
+    */
     cy.get(".savediv2").click();
-
-    // Alert bar appear saying "Saved. Result is at the bottom.  Use this link to share or edit with your friends:  (link)"
     cy.on("window:alert", (Text) => {
       expect(Text).to.contains(
         "Saved. Result is at the bottom.  Use this link to share or edit with your friends"
       );
     });
 
-    // Calculation is done and a box appears saying:
-    // Person2 owns 50 to Person1
     cy.get(".the-return .ergi")
       .eq(0)
       .find(".deb")
@@ -326,7 +341,6 @@ describe("My Second Test Suite", function () {
       .find("div.cred")
       .should("have.text", "Person1");
 
-    // Person3 owns 50 to Person1
     cy.get(".the-return .ergi")
       .eq(1)
       .find(".deb")
@@ -341,12 +355,18 @@ describe("My Second Test Suite", function () {
       .should("have.text", "Person1");
   });
 
-  it("Nominal case: Both blue button (Calculate & Save!) and orange bar appears when changing expense name", function () {
-    // Click on Button (submitbutton!)
+  it("4.Nominal case: Both blue button (Calculate & Save!) and orange bar appears when changing expense name", function () {
+    // 4.1: Click on "Calculate & Save!"
+    /*
+    Expected result:
+    Calculation is done and a box appears saying:
+      - Person2 owns 33.33 to Person1
+      - Person3 owns 33.33 to Person1
+
+    Note: We had to do it so that we can show the clicking on "more" will activate the orange bar and the blue button
+    */
     cy.get("#submitbutton").click();
 
-    // Calculation is done and a box appears saying:
-    // Person2 owns 33.33 to Person1
     cy.get(".the-return .ergi")
       .eq(0)
       .find(".deb")
@@ -360,7 +380,6 @@ describe("My Second Test Suite", function () {
       .find("div.cred")
       .should("have.text", "Person1");
 
-    // Person3 owns 33.33 to Person1
     cy.get(".the-return .ergi")
       .eq(1)
       .find(".deb")
@@ -374,13 +393,11 @@ describe("My Second Test Suite", function () {
       .find("div.cred")
       .should("have.text", "Person1");
 
-    // Change the first expense name of Person1 from "Ticket" to "food"
+    // 4.2: Change the first expense name of Person1 from "Ticket" to "food"
+    // Expected result: Both blue button and orange bar reappear
     cy.get(".calc .inputwrap").eq(0).find("input.was").clear().type("Food");
 
-    // Button (submitbutton!) appears
     cy.get("#submitbutton");
-
-    // Find the orange bar
     cy.get(".savediv2")
       .find(".save2")
       .should(
@@ -389,18 +406,22 @@ describe("My Second Test Suite", function () {
       )
       .click();
 
-    // Click on the orange bar
-    cy.get(".savediv2").click();
+    // 4.3: Click on the orange bar
+    /*
+    Expected result:
+    Alert bar appear saying "Saved. Result is at the bottom.  Use this link to share or edit with your friends:  (link)"
 
-    // Alert bar appear saying "Saved. Result is at the bottom.  Use this link to share or edit with your friends:  (link)"
+    Calculation is done and a box appears saying:
+      - Person2 owns 33.33 to Person1
+      - Person3 owns 33.33 to Person1
+    */
+    cy.get(".savediv2").click();
     cy.on("window:alert", (Text) => {
       expect(Text).to.contains(
         "Saved. Result is at the bottom.  Use this link to share or edit with your friends"
       );
     });
 
-    // Calculation is done and a box appears saying:
-    // Person2 owns 33.33 to Person1
     cy.get(".the-return .ergi")
       .eq(0)
       .find(".deb")
@@ -414,7 +435,6 @@ describe("My Second Test Suite", function () {
       .find("div.cred")
       .should("have.text", "Person1");
 
-    // Person3 owns 33.33 to Person1
     cy.get(".the-return .ergi")
       .eq(1)
       .find(".deb")
@@ -430,11 +450,17 @@ describe("My Second Test Suite", function () {
   });
 
   it("Nominal case: Both blue button (Calculate & Save!) and orange bar appears when changing person name", function () {
-    // Click on Button (submitbutton!)
+    // 5.1: Click on "Calculate & Save!"
+    /*
+    Expected result:
+    Calculation is done and a box appears saying:
+      - Person2 owns 33.33 to Person1
+      - Person3 owns 33.33 to Person1
+
+    Note: We had to do it so that we can show the clicking on "more" will activate the orange bar and the blue button
+    */
     cy.get("#submitbutton").click();
 
-    // Calculation is done and a box appears saying:
-    // Person2 owns 33.33 to Person1
     cy.get(".the-return .ergi")
       .eq(0)
       .find(".deb")
@@ -448,7 +474,6 @@ describe("My Second Test Suite", function () {
       .find("div.cred")
       .should("have.text", "Person1");
 
-    // Person3 owns 33.33 to Person1
     cy.get(".the-return .ergi")
       .eq(1)
       .find(".deb")
@@ -462,13 +487,11 @@ describe("My Second Test Suite", function () {
       .find("div.cred")
       .should("have.text", "Person1");
 
-    // Type in Person's name in form 1: Guy1
+    // 5.2: Change the first person name of Person1 from "Person1" to "Guy1"
+    // Expected result: Both blue button and orange bar reappear
     cy.get(".calc .userwrap").eq(0).find("input.name").clear().type("Guy1");
 
-    // Button (submitbutton!) appears
     cy.get("#submitbutton");
-
-    // Find the orange bar
     cy.get(".savediv2")
       .find(".save2")
       .should(
@@ -477,18 +500,23 @@ describe("My Second Test Suite", function () {
       )
       .click();
 
-    // Click on the orange bar
-    cy.get(".savediv2").click();
+    // 5.3: Click on the orange bar
+    /*
+    Expected result:
+    Alert bar appear saying "Saved. Result is at the bottom.  Use this link to share or edit with your friends:  (link)""
 
-    // Alert bar appear saying "Saved. Result is at the bottom.  Use this link to share or edit with your friends:  (link)"
+
+    Calculation is done and a box appears saying:
+      - Person2 owns 33.33 to Person1
+      - Person3 owns 33.33 to Person1
+    */
+    cy.get(".savediv2").click();
     cy.on("window:alert", (Text) => {
       expect(Text).to.contains(
         "Saved. Result is at the bottom.  Use this link to share or edit with your friends"
       );
     });
 
-    // Calculation is done and a box appears saying:
-    // Person2 owns 33.33 to Person1
     cy.get(".the-return .ergi")
       .eq(0)
       .find(".deb")
@@ -502,7 +530,6 @@ describe("My Second Test Suite", function () {
       .find("div.cred")
       .should("have.text", "Guy1");
 
-    // Person3 owns 33.33 to Person1
     cy.get(".the-return .ergi")
       .eq(1)
       .find(".deb")
