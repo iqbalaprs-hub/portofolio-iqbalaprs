61841cc47795cfd1ac982b0db7553fb990d6d2b3
ronyhourani <ronyhourani@gmail.com>
2023-09-20 05:45:44 +0300

    [Portf/Billzer] link.js ameliorated

--- projects/qa/cypress/cypress/integration/billzer/link.js
+++ projects/qa/cypress/cypress/integration/billzer/link.js
@@ -2,40 +2,41 @@
 
 describe("My Second Test Suite", function () {
   beforeEach(() => {
+    // Prereq: the user visits the page "https://billzer.com/"
     cy.visit("https://billzer.com/");
-    // Default number of member is 3
-    cy.get(".input-hg").should("have.value", "3");
-    // Type in the Name input: Trip
-    cy.get("#text").type("Trip");
 
-    // Click the button GO
+    /* 
+    Prereq:
+    The user typed in the Main page the following information:
+    Member count input: 3
+    Name input: Trip
+    */
+    cy.get(".input-hg").should("have.value", "3");
+    cy.get("#text").type("Trip");
     cy.get("#go").click();
 
-    // At the top of the page, I see the name "Trip"
-    // There are 3 members forms to fill
-    cy.get("#occasion").contains("Trip");
-    cy.get(".calc").find(".userwrap").should("have.length", 3);
-
-    // Type in Person's name in form 1: Person1
+    /*
+    Prereq:
+    In the calculation page, the user:
+    - Type in Person's name in form 1: Person1
+    - Type in Person's name in form 2: Person2
+    - Type in Person's name in form 3: Person3
+    - Type in expense's name in form1: Ticket
+    - Type in expense's name in form2: Ticket
+    - Type in expense's name in form3: Ticket
+    - Type in the expense amount of the "ticket" in Person1: 100
+    */
     cy.get(".calc .userwrap").eq(0).find("input.name").type("Person1");
-    // Type in Person's name in form 2: Person2
     cy.get(".calc .userwrap").eq(1).find("input.name").type("Person2");
-    // Type in Person's name in form 3: Person3
     cy.get(".calc .userwrap").eq(2).find("input.name").type("Person3");
-
-    // Type in expense's name in form1: Ticket
     cy.get(".calc .inputwrap").eq(0).find("input.was").type("Ticket");
-    // Type in expense's name in form2: Ticket
     cy.get(".calc .inputwrap").eq(1).find("input.was").type("Ticket");
-    // Type in expense's name in form3: Ticket
     cy.get(".calc .inputwrap").eq(2).find("input.was").type("Ticket");
-
-    // Type in expense's amount in Person1: 100
     cy.get(".calc .inputwrap").eq(0).find("input.price").type("100");
   });
 
-  it("Nominal case: User gets the link of the calculation form the green sentence replacing the orange bar", function () {
-    // Find the orange bar
+  it("2.Nominal case: User gets the link of the calculation form the green sentence replacing the orange bar", function () {
+    // 2.1: Find the orange bar
     cy.get(".savediv2")
       .find(".save2")
       .should(
@@ -43,19 +44,22 @@ describe("My Second Test Suite", function () {
         "Unsaved changes. Hit the calculate button below or click here."
       )
       .click();
-
-    // Click on the orange bar
     cy.get(".savediv2").click();
 
-    // Alert bar appear saying "Saved. Result is at the bottom.  Use this link to share or edit with your friends:  (link)"
+    // 2.1: Expected result: Alert appears saying that it is saved and carry a link of the page
     cy.on("window:alert", (Text) => {
       expect(Text).to.contains(
         "Saved. Result is at the bottom.  Use this link to share or edit with your friends"
       );
     });
 
-    // Calculation is done and a box appears saying:
-    // Person2 owns 33.33 to Person1
+    /*
+    2.1: Expected result:
+    calculation us done and box appears saying:
+      -Person2 owns 33.3 to person1
+      -Person3 owns 33.33 to Person1
+    */
+
     cy.get(".the-return .ergi")
       .eq(0)
       .find(".deb")
@@ -69,7 +73,6 @@ describe("My Second Test Suite", function () {
       .find("div.cred")
       .should("have.text", "Person1");
 
-    // Person3 owns 33.33 to Person1
     cy.get(".the-return .ergi")
       .eq(1)
       .find(".deb")
@@ -83,8 +86,8 @@ describe("My Second Test Suite", function () {
       .find("div.cred")
       .should("have.text", "Person1");
 
-    // Copy the link from the green sentence, compare it to the  URL
-    // The link extracted and the URL are the same
+    // 2.2: Copy the link from the green sentence, compare it to the  URL
+    // 2.2: Expected result: The link extracted and the URL are the same
     cy.get(".savediv")
       .find(".save")
       .invoke("text")
