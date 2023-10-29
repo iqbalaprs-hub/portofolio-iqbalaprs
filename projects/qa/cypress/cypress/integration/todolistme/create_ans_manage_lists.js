0ddcce17dd1a053e54246ec94cb31a4e720ecd5b
ronyhourani <ronyhourani@gmail.com>
2023-10-29 08:33:09 +0200

    [Portf/Todolist] Create 1st testCase for 1st test Suite

--- projects/qa/cypress/cypress/integration/todolistme/create_ans_manage_lists.js
+++ projects/qa/cypress/cypress/integration/todolistme/create_ans_manage_lists.js
@@ -24,5 +24,42 @@ describe("Feature: Create and manage lists", () => {
       .click();
   });
 
-  it("", () => {});
+  it("1- Nominal case: The user can use 5 lists already present by default in the web application ", () => {
+    /*
+    1.1: Expected result: 5 lists are present by default in the web application:
+        - Today’s tasks
+        - Projects
+        - Things to do later
+        - Example template
+        - Important info
+    */
+
+    // Check if there are 6 files ('Test todo list' included)
+    cy.get("#listmanager #lists #container_0")
+      .find("li")
+      .should("have.length", 6);
+
+    // Check each file's name
+    cy.get("#listmanager #lists #mycategory_0 #container_0 li:eq(0)")
+      .find(".listname")
+      .should("have.text", "Today's Tasks ");
+    cy.get("#listmanager #lists #mycategory_0 #container_0 li:eq(1)")
+      .find(".listname")
+      .should("have.text", "Projects ");
+    cy.get("#listmanager #lists #mycategory_0 #container_0 li:eq(2)")
+      .find(".listname")
+      .should("have.text", "Things to do later ");
+    cy.get("#listmanager #lists #mycategory_0 #container_0 li:eq(3)")
+      .find(".listname")
+      .should("have.text", "Example template ");
+    cy.get("#listmanager #lists #mycategory_0 #container_0 li:eq(4)")
+      .find(".listname")
+      .should("have.text", "Important Info ");
+    cy.get("#listmanager #lists #mycategory_0 #container_0 li:eq(5)")
+      .find(".listname")
+      .should("have.text", "Test todo list ");
+    cy.log(
+      "You have 5 initial files and 1 file we just created called 'Test todo list'"
+    );
+  });
 });
