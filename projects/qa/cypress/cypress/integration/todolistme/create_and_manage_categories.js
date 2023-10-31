84631d9e0534cf1aaa7d6f643ec84e616ee91515
ronyhourani <ronyhourani@gmail.com>
2023-10-31 13:13:14 +0200

    [Portf/Todolist] Create 1st testCase for 2st testSuite

--- projects/qa/cypress/cypress/integration/todolistme/create_and_manage_categories.js
+++ projects/qa/cypress/cypress/integration/todolistme/create_and_manage_categories.js
@@ -24,5 +24,40 @@ describe("Feature: create and manage categories", () => {
       .click();
   });
 
-  it("My SecondTest case", function () {});
+  it("1- Nominal case: The user can create categories", () => {
+    // 1.1: Click on the icon "add new category"
+    cy.get("img.adddivider").click();
+
+    // 1.2: Click on the "save" button
+    cy.get("#inplaceeditor").find('input[type="submit"]').click();
+    // Expected return: A new category is created. It is named "New Category" (which is the default name)
+    // Check if there is only 1 category
+    cy.get("#lists #mycategories ").find("li").should("have.length", 1);
+    // Check if the name of the new file is "New Category"
+    cy.get("#lists #mycategories li:eq(0)")
+      .find("span")
+      .should("have.text", "New Category");
+
+    // 1.3: Click on the icon "add new category"
+    cy.get("img.adddivider").click();
+
+    // 1.4: Click on the "save" button
+    cy.get("#inplaceeditor").find('input[type="submit"]').click();
+    // Expected return: A new category is created. It is named "New Category" (which is the default name)
+    // Check if there are 2 categories
+    cy.get("#lists #mycategories ").find("li").should("have.length", 2);
+    // Check if the name of the new file is "New Category"
+    cy.get("#lists #mycategories li:eq(1)")
+      .find("span")
+      .should("have.text", "New Category");
+
+    // 1.5: Click on the icon "add new category"
+    cy.get("img.adddivider").click();
+
+    // 1.6: Click on the "cancel" button
+    cy.get("#inplaceeditor").find('input[type="button"]').click();
+    // Expected return: The category was not created
+    // Check if there are STILL 2 categories
+    cy.get("#lists #mycategories ").find("li").should("have.length", 2);
+  });
 });
