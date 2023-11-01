ebabc95cae857a5fbaa38810d1f34032f2c13ac5
ronyhourani <ronyhourani@gmail.com>
2023-11-01 10:22:40 +0200

    [Portf/Todolist] Create 2th testCase for 3th testSuite

--- projects/qa/cypress/cypress/integration/todolistme/create_rename_delete_and_dragItems.js
+++ projects/qa/cypress/cypress/integration/todolistme/create_rename_delete_and_dragItems.js
@@ -60,4 +60,100 @@ describe("Feature: Create, rename, delete and drag items", () => {
       .find('input[type="checkbox"]')
       .should("not.be.checked");
   });
+
+  it("2- Nominal case: The user can rename list items ", () => {
+    // 2.1: Create new item in the To-do-items: Task1
+    cy.get("#additempanel").find("#newtodo").type("Task1").type("{enter}");
+    // Expected result: A new item called "Task1" in the To-do-items.The new item "Task1" is unchecked.
+    cy.get("#todolistpanel #todo_0")
+      .find("span#mytodo_0")
+      .should("have.text", "Task1");
+    cy.get("#todolistpanel #todo_0")
+      .find('input[type="checkbox"]')
+      .should("not.be.checked");
+
+    // 2.2: Double-click on the item "Task1" and name it "Item1"
+    cy.get("#todolistpanel #mytodos #todo_0").dblclick();
+    cy.get("#todolistpanel #mytodos #todo_0")
+      .find("input[type=text]")
+      .type("Item1");
+    // 2.3: Click "save" button
+    cy.get("#todolistpanel #mytodos #todo_0")
+      .find("input[type=submit]")
+      .click();
+    // Expected result: The item "Task1" is now named "Item1"
+    cy.get("#todolistpanel #todo_0")
+      .find("span#mytodo_0")
+      .should("have.text", "Item1");
+
+    // 2.4: Double-click on the item "Item1" and name it "Task1"
+    cy.get("#todolistpanel #mytodos #todo_0").dblclick();
+    cy.get("#todolistpanel #mytodos #todo_0")
+      .find("input[type=text]")
+      .type("Task1");
+
+    // 2.5: Click "cancel" button
+    cy.get("#todolistpanel #mytodos #todo_0")
+      .find("input[type=button]")
+      .click();
+    // Expected result: The item is still named "Item1"
+    cy.get("#todolistpanel #todo_0")
+      .find("span#mytodo_0")
+      .should("have.text", "Item1");
+
+    // 2.6: Create new item in the To-do-items: Task2
+    cy.get("#additempanel").find("#newtodo").type("Task2").type("{enter}");
+    // Expected result: A new item called "Task2" in the To-do-items.The new item "Task2" is unchecked
+    cy.get("#todolistpanel #todo_1")
+      .find("span#mytodo_1")
+      .should("have.text", "Task2");
+    cy.get("#todolistpanel #todo_1")
+      .find('input[type="checkbox"]')
+      .should("not.be.checked");
+
+    // 2.7: Check the item "Task2"
+    cy.get("#todolistpanel #mytodos #todo_1")
+      .find('input[type="checkbox"]')
+      .click();
+    // Expected result: The item "Task2" is in the done-items. "Task2" is checked
+    cy.get("#doneitemspanel #mydonetodos #todo_1")
+      .find("span#mytodo_1")
+      .should("have.text", "Task2");
+    cy.get("#doneitemspanel #mydonetodos #todo_1")
+      .find('input[type="checkbox"]')
+      .should("be.checked");
+
+    // 2.8: Double-click on the item "Task2" and name it "Item2"
+    cy.get("#doneitemspanel #mydonetodos #todo_1").dblclick();
+    cy.get("#doneitemspanel #mydonetodos #todo_1")
+      .find("input[type=text]")
+      .type("Item2");
+
+    // 2.9: Click "save" button
+    cy.get("#doneitemspanel #mydonetodos #todo_1")
+      .find("input[type=submit]")
+      .click();
+    // Expected result: The item "Task2" is now named "Item2"
+    cy.get("#doneitemspanel #mydonetodos #todo_1")
+      .find("span#mytodo_1")
+      .should("have.text", "Item2");
+
+    // 2.10: Double-click on the item "Item2" and name it "Task2"
+    cy.get("#doneitemspanel #mydonetodos #todo_1").dblclick();
+    cy.get("#doneitemspanel #mydonetodos #todo_1")
+      .find("input[type=text]")
+      .type("Task2");
+
+    // 2.11: Click "cancel" button
+    cy.get("#doneitemspanel #mydonetodos #todo_1")
+      .find("input[type=button]")
+      .click();
+    // Expected result: The item is still named "Item2"
+    cy.get("#doneitemspanel #mydonetodos #todo_1")
+      .find("span#mytodo_1")
+      .should("have.text", "Item2");
+
+    // 2.12: Create new item in the To-do-items: Task3
+    cy.get("#additempanel").find("#newtodo").type("Task3").type("{enter}");
+  });
 });
