cc2fcb5e866088186087267fee429373e1e396af
ronyhourani <ronyhourani@gmail.com>
2023-11-04 22:08:40 +0200

    [Portf/Todolist] Create 1th testCase for 4th testSuite

--- projects/qa/cypress/cypress/integration/todolistme/sorting_list_items.js
+++ projects/qa/cypress/cypress/integration/todolistme/sorting_list_items.js
@@ -24,5 +24,174 @@ describe("Feature: Sorting list items", () => {
       .click();
   });
 
-  it("My FirstTest case", () => {});
+  it("1- Nominal case: The user can sort items in the to-do-items", () => {
+    // 1.1: Create new item in the To-do-list: e
+    cy.get("#additempanel").find("#newtodo").type("e").type("{enter}");
+
+    // 1.2: Create new item in the To-do-list: b
+    cy.get("#additempanel").find("#newtodo").type("b").type("{enter}");
+
+    // 1.3: Create new item in the To-do-list: k
+    cy.get("#additempanel").find("#newtodo").type("k").type("{enter}");
+
+    // 1.4: Create new item in the To-do-list: a
+    cy.get("#additempanel").find("#newtodo").type("a").type("{enter}");
+
+    // 1.5: Create new item in the To-do-list: i
+    cy.get("#additempanel").find("#newtodo").type("i").type("{enter}");
+
+    // 1.6: Create new item in the To-do-list: x
+    cy.get("#additempanel").find("#newtodo").type("x").type("{enter}");
+    // Expected result: The order of the items for top to bottom is: e, b, k, a, i, x)
+    cy.get("#todolistpanel #mytodos").find("li").should("have.length", 6);
+    cy.get("#todolistpanel #mytodos li")
+      .eq(0)
+      .find("span")
+      .should("have.text", "e");
+    cy.get("#todolistpanel #mytodos li")
+      .eq(1)
+      .find("span")
+      .should("have.text", "b");
+    cy.get("#todolistpanel #mytodos li")
+      .eq(2)
+      .find("span")
+      .should("have.text", "k");
+    cy.get("#todolistpanel #mytodos li")
+      .eq(3)
+      .find("span")
+      .should("have.text", "a");
+    cy.get("#todolistpanel #mytodos li")
+      .eq(4)
+      .find("span")
+      .should("have.text", "i");
+    cy.get("#todolistpanel #mytodos li")
+      .eq(5)
+      .find("span")
+      .should("have.text", "x");
+
+    // 1.7: Drag the item "e" to be the bottom item in the To-do-items
+    cy.get("#todolistpanel #todo_0").drag("#todolistpanel #todo_5", {
+      source: { x: 0, y: 0 }, // applies to the element being dragged
+      target: { position: "bottom" }, // applies to the drop target
+      force: true, // applied to both the source and target element
+    });
+    // Expected result: The item "e" is the last item in the To-do-items (from top to bottom:  b, k, a, i, x, e)
+    cy.get("#todolistpanel #mytodos li")
+      .eq(0)
+      .find("span")
+      .should("have.text", "b");
+    cy.get("#todolistpanel #mytodos li")
+      .eq(1)
+      .find("span")
+      .should("have.text", "k");
+    cy.get("#todolistpanel #mytodos li")
+      .eq(2)
+      .find("span")
+      .should("have.text", "a");
+    cy.get("#todolistpanel #mytodos li")
+      .eq(3)
+      .find("span")
+      .should("have.text", "i");
+    cy.get("#todolistpanel #mytodos li")
+      .eq(4)
+      .find("span")
+      .should("have.text", "x");
+    cy.get("#todolistpanel #mytodos li")
+      .eq(5)
+      .find("span")
+      .should("have.text", "e");
+
+    // 1.8: Choose the option "Alphabetical" to sort the items alphabetically
+    cy.get("#sortselect").find("#sort1").click();
+    cy.wait(1000);
+    // Expected result: The items are sorted alphabetically (from top to bottom: a, b, e, i, k, x)
+    cy.get("#todolistpanel #mytodos li")
+      .eq(0)
+      .find("span")
+      .should("have.text", "a");
+    cy.get("#todolistpanel #mytodos li")
+      .eq(1)
+      .find("span")
+      .should("have.text", "b");
+    cy.get("#todolistpanel #mytodos li")
+      .eq(2)
+      .find("span")
+      .should("have.text", "e");
+    cy.get("#todolistpanel #mytodos li")
+      .eq(3)
+      .find("span")
+      .should("have.text", "i");
+    cy.get("#todolistpanel #mytodos li")
+      .eq(4)
+      .find("span")
+      .should("have.text", "k");
+    cy.get("#todolistpanel #mytodos li")
+      .eq(5)
+      .find("span")
+      .should("have.text", "x");
+
+    // 1.9: Choose the option "Random" to sort the items randomly
+    cy.get("#sortselect").find("#sort2").click();
+    // Expected result: The items are no longer sorted alphabetically
+    cy.get("#todolistpanel #mytodos li")
+      .find("span") // Find the <span> elements within the <li> elements
+      .invoke("text")
+      .should("not.deep.equal", ["a", "b", "e", "i", "k", "x"]);
+
+    // 1.10: Choose the option "Normal" to sort the items
+    cy.get("#sortselect").find("#sort0").click();
+    // Expected result: The items are sorted as they were before step 1.8  (from top to bottom: b, k, a, i, x, e)
+    cy.get("#todolistpanel #mytodos li")
+      .eq(0)
+      .find("span")
+      .should("have.text", "b");
+    cy.get("#todolistpanel #mytodos li")
+      .eq(1)
+      .find("span")
+      .should("have.text", "k");
+    cy.get("#todolistpanel #mytodos li")
+      .eq(2)
+      .find("span")
+      .should("have.text", "a");
+    cy.get("#todolistpanel #mytodos li")
+      .eq(3)
+      .find("span")
+      .should("have.text", "i");
+    cy.get("#todolistpanel #mytodos li")
+      .eq(4)
+      .find("span")
+      .should("have.text", "x");
+    cy.get("#todolistpanel #mytodos li")
+      .eq(5)
+      .find("span")
+      .should("have.text", "e");
+
+    // 1.11: Choose the option "Top3" to select the top 3 items (from top to downward) on the To-do-items
+    cy.get("#sortselect").find("#sort3").click();
+    // Expected result: The top 3 items are colored in black (b,k,a). The other items are colored in light grey (i,x,e)
+    cy.get("#todolistpanel #mytodos li")
+      .eq(0)
+      .find("span")
+      .should("have.css", "color", "rgb(51, 51, 51)");
+    cy.get("#todolistpanel #mytodos li")
+      .eq(1)
+      .find("span")
+      .should("have.css", "color", "rgb(51, 51, 51)");
+    cy.get("#todolistpanel #mytodos li")
+      .eq(2)
+      .find("span")
+      .should("have.css", "color", "rgb(51, 51, 51)");
+    cy.get("#todolistpanel #mytodos li")
+      .eq(3)
+      .find("span")
+      .should("have.css", "color", "rgb(221, 221, 221)");
+    cy.get("#todolistpanel #mytodos li")
+      .eq(4)
+      .find("span")
+      .should("have.css", "color", "rgb(221, 221, 221)");
+    cy.get("#todolistpanel #mytodos li")
+      .eq(5)
+      .find("span")
+      .should("have.css", "color", "rgb(221, 221, 221)");
+  });
 });
