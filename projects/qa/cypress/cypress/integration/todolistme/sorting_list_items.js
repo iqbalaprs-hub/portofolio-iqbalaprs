e8e5893580b54923e639eb69951484a0b8a6a414
ronyhourani <ronyhourani@gmail.com>
2023-11-04 22:29:48 +0200

    [Portf/Todolist] Create 2th testCase for 4th testSuite

--- projects/qa/cypress/cypress/integration/todolistme/sorting_list_items.js
+++ projects/qa/cypress/cypress/integration/todolistme/sorting_list_items.js
@@ -194,4 +194,133 @@ describe("Feature: Sorting list items", () => {
       .find("span")
       .should("have.css", "color", "rgb(221, 221, 221)");
   });
+
+  it("2- Nominal case: The user can sort items in the done-items by dragging items", () => {
+    // 2.1: Create new item in the To-do-list: e
+    cy.get("#additempanel").find("#newtodo").type("e").type("{enter}");
+
+    // 2.2: Create new item in the To-do-list: b
+    cy.get("#additempanel").find("#newtodo").type("b").type("{enter}");
+
+    // 2.3: Create new item in the To-do-list: k
+    cy.get("#additempanel").find("#newtodo").type("k").type("{enter}");
+
+    // 2.4: Create new item in the To-do-list: a
+    cy.get("#additempanel").find("#newtodo").type("a").type("{enter}");
+
+    // 2.5: Create new item in the To-do-list: i
+    cy.get("#additempanel").find("#newtodo").type("i").type("{enter}");
+
+    // 2.6: Create new item in the To-do-list: x
+    cy.get("#additempanel").find("#newtodo").type("x").type("{enter}");
+    // Expected result: The order of the items for top to bottom is: e, b, k, a, i, x)
+    cy.get("#todolistpanel #mytodos").find("li").should("have.length", 6);
+    cy.get("#todolistpanel #mytodos li")
+      .eq(0)
+      .find("span")
+      .should("have.text", "e");
+    cy.get("#todolistpanel #mytodos li")
+      .eq(1)
+      .find("span")
+      .should("have.text", "b");
+    cy.get("#todolistpanel #mytodos li")
+      .eq(2)
+      .find("span")
+      .should("have.text", "k");
+    cy.get("#todolistpanel #mytodos li")
+      .eq(3)
+      .find("span")
+      .should("have.text", "a");
+    cy.get("#todolistpanel #mytodos li")
+      .eq(4)
+      .find("span")
+      .should("have.text", "i");
+    cy.get("#todolistpanel #mytodos li")
+      .eq(5)
+      .find("span")
+      .should("have.text", "x");
+
+    // 2.7: Ckeck all items (from bottom to top)
+    cy.get("#todolistpanel #mytodos li")
+      .eq(5)
+      .find('input[type="checkbox"]')
+      .click();
+    cy.get("#todolistpanel #mytodos li")
+      .eq(4)
+      .find('input[type="checkbox"]')
+      .click();
+    cy.get("#todolistpanel #mytodos li")
+      .eq(3)
+      .find('input[type="checkbox"]')
+      .click();
+    cy.get("#todolistpanel #mytodos li")
+      .eq(2)
+      .find('input[type="checkbox"]')
+      .click();
+    cy.get("#todolistpanel #mytodos li")
+      .eq(1)
+      .find('input[type="checkbox"]')
+      .click();
+    cy.get("#todolistpanel #mytodos li")
+      .eq(0)
+      .find('input[type="checkbox"]')
+      .click();
+    // Expected result: All items moved from To-do-items to Done-items. The order of the items for top to bottom is: e, b, k, a, i, x)
+    cy.get("#doneitemspanel #mydonetodos li")
+      .eq(0)
+      .find("span")
+      .should("have.text", "e");
+    cy.get("#doneitemspanel #mydonetodos li")
+      .eq(1)
+      .find("span")
+      .should("have.text", "b");
+    cy.get("#doneitemspanel #mydonetodos li")
+      .eq(2)
+      .find("span")
+      .should("have.text", "k");
+    cy.get("#doneitemspanel #mydonetodos li")
+      .eq(3)
+      .find("span")
+      .should("have.text", "a");
+    cy.get("#doneitemspanel #mydonetodos li")
+      .eq(4)
+      .find("span")
+      .should("have.text", "i");
+    cy.get("#doneitemspanel #mydonetodos li")
+      .eq(5)
+      .find("span")
+      .should("have.text", "x");
+
+    // 2.8: Drag the item "e" to be the bottom item in the done-items
+    cy.get("#doneitemspanel #todo_0").drag("#doneitemspanel #todo_5", {
+      source: { x: 0, y: 0 }, // applies to the element being dragged
+      target: { position: "bottom" }, // applies to the drop target
+      force: true, // applied to both the source and target element
+    });
+    // Expected result: The item "e" is the last item in the To-do-items (from top to bottom:  b, k, a, i, x, e)
+    cy.get("#doneitemspanel #mydonetodos li")
+      .eq(0)
+      .find("span")
+      .should("have.text", "b");
+    cy.get("#doneitemspanel #mydonetodos li")
+      .eq(1)
+      .find("span")
+      .should("have.text", "k");
+    cy.get("#doneitemspanel #mydonetodos li")
+      .eq(2)
+      .find("span")
+      .should("have.text", "a");
+    cy.get("#doneitemspanel #mydonetodos li")
+      .eq(3)
+      .find("span")
+      .should("have.text", "i");
+    cy.get("#doneitemspanel #mydonetodos li")
+      .eq(4)
+      .find("span")
+      .should("have.text", "x");
+    cy.get("#doneitemspanel #mydonetodos li")
+      .eq(5)
+      .find("span")
+      .should("have.text", "e");
+  });
 });
