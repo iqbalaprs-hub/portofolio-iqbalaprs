a983a6713ac532d01ac0b1c967c6b0eb473ff525
ronyhourani <ronyhourani@gmail.com>
2023-11-08 07:10:27 +0200

    [Portf/Todolist] Create 1th testCase for 4th testSuite

--- projects/qa/cypress/cypress/integration/todolistme/sorting_list_items.js
+++ projects/qa/cypress/cypress/integration/todolistme/sorting_list_items.js
@@ -23,4 +23,39 @@ describe("Feature: Sorting list items", () => {
       .parent("li")
       .click();
   });
+
+  it("1- Nominal case: The user can sort items in the to-do-items by dragging items", () => {
+    // 1.1: Create new item in the To-do-list: e
+    cy.get("#additempanel").find("#newtodo").type("e").type("{enter}");
+
+    // 1.2: Create new item in the To-do-list: b
+    cy.get("#additempanel").find("#newtodo").type("b").type("{enter}");
+
+    // 1.3: Create new item in the To-do-list: k
+    cy.get("#additempanel").find("#newtodo").type("k").type("{enter}");
+
+    // 1.4: Create new item in the To-do-list: a
+    cy.get("#additempanel").find("#newtodo").type("a").type("{enter}");
+
+    // 1.5: Create new item in the To-do-list: i
+    cy.get("#additempanel").find("#newtodo").type("i").type("{enter}");
+
+    // 1.6: Create new item in the To-do-list: x
+    cy.get("#additempanel").find("#newtodo").type("x").type("{enter}");
+
+    // 1.7: Drag the item "e" to be the bottom item in the To-do-items
+    // Exxpected result: The item "e" is the last item in the To-do-items (from top to bottom:  b, k, a, i, x, e)
+    cy.get("#todolistpanel #mytodos #todo_0").drag(
+      "#todolistpanel #mytodos #todo_5"
+    );
+    cy.get("#todolistpanel #mytodos li").should(($lis) => {
+      // Convert the list items into an array of their text content
+      const textArray = $lis.map((index, el) => Cypress.$(el).text()).get();
+
+      // Define the expected order
+      const expectedOrder = ["b", "k", "a", "i", "x", "e"];
+
+      expect(textArray).to.deep.equal(expectedOrder);
+    });
+  });
 });
