8b1df69830091f69848a06e4537a998317325c77
ronyhourani <ronyhourani@gmail.com>
2023-11-17 20:05:06 +0200

    [Portf/RC] Fix and ameliorate language

--- projects/qa/cypress/cypress/integration/restCountries/language.js
+++ projects/qa/cypress/cypress/integration/restCountries/language.js
@@ -1,7 +1,7 @@
 /// <reference types="Cypress" />
 
 describe("API test for endpoint GET /lang/{language}", () => {
-  it("1- Test the property 'languages' with endpoint, which is the language's name, as a basic normal endpoint", () => {
+  it("1- Test the property 'languages' with basic normal endpoint", () => {
     cy.request("GET", "https://restcountries.com/v3.1/lang/Russian").then(
       (response) => {
         // Assertion 1: Response is 200
@@ -9,6 +9,7 @@ describe("API test for endpoint GET /lang/{language}", () => {
 
         // Assertion 2: The array contains 7 objects (Seven countries)
         cy.wrap(response.body).should("have.length", 7);
+        cy.log("There are 7 countries");
 
         /*
             Assertion 3:
@@ -21,46 +22,21 @@ describe("API test for endpoint GET /lang/{language}", () => {
                 - Kyrgyzstan
                 - Belarus
         */
-        const kyrgyzstan = response.body[0];
-        expect(kyrgyzstan.name.common).to.equal("Kyrgyzstan");
 
-        const russia = response.body[1];
-        expect(russia.name.common).to.equal("Russia");
+        response.body.forEach((country, index) => {
+          // Access the different properties
+          const commonName = country.name.common;
+          const language = country.languages;
 
-        const uzbekistan = response.body[2];
-        expect(uzbekistan.name.common).to.equal("Uzbekistan");
-
-        const kazakhstan = response.body[3];
-        expect(kazakhstan.name.common).to.equal("Kazakhstan");
-
-        const turkmenistan = response.body[4];
-        expect(turkmenistan.name.common).to.equal("Turkmenistan");
-
-        const belarus = response.body[5];
-        expect(belarus.name.common).to.equal("Belarus");
-
-        const tajikistan = response.body[6];
-        expect(tajikistan.name.common).to.equal("Tajikistan");
-
-        cy.log(JSON.stringify(kyrgyzstan.name.common));
-        cy.log(JSON.stringify(kyrgyzstan.languages));
-        cy.log(JSON.stringify(russia.name.common));
-        cy.log(JSON.stringify(russia.languages));
-        cy.log(JSON.stringify(uzbekistan.name.common));
-        cy.log(JSON.stringify(uzbekistan.languages));
-        cy.log(JSON.stringify(kazakhstan.name.common));
-        cy.log(JSON.stringify(kazakhstan.languages));
-        cy.log(JSON.stringify(turkmenistan.name.common));
-        cy.log(JSON.stringify(turkmenistan.languages));
-        cy.log(JSON.stringify(belarus.name.common));
-        cy.log(JSON.stringify(belarus.languages));
-        cy.log(JSON.stringify(tajikistan.name.common));
-        cy.log(JSON.stringify(tajikistan.languages));
+          cy.log(`Common Name ${index + 1}: ${commonName}`);
+          cy.log(`language ${index + 1}:`);
+          cy.log(JSON.stringify(language));
+        });
       }
     );
   });
 
-  it("2- Test the property 'languages' by showing that the endpoint, which is the language's name, is case-insensitive and it gives more than one country", () => {
+  it("2- Test the property 'languages' by showing that the endpoint performs a case-insensitive match against the country's languages and it gives more than one country", () => {
     cy.request("GET", "https://restcountries.com/v3.1/lang/rUssIan").then(
       (response) => {
         // Assertion 1: Response is 200
@@ -68,6 +44,7 @@ describe("API test for endpoint GET /lang/{language}", () => {
 
         // Assertion 2: The array contains 7 objects (Seven countries)
         cy.wrap(response.body).should("have.length", 7);
+        cy.log("There are 7 countries");
 
         /*
              Assertion 3:
@@ -80,46 +57,20 @@ describe("API test for endpoint GET /lang/{language}", () => {
                  - Kyrgyzstan
                  - Belarus
          */
-        const kyrgyzstan = response.body[0];
-        expect(kyrgyzstan.name.common).to.equal("Kyrgyzstan");
+        response.body.forEach((country, index) => {
+          // Access the different properties
+          const commonName = country.name.common;
+          const language = country.languages;
 
-        const russia = response.body[1];
-        expect(russia.name.common).to.equal("Russia");
-
-        const uzbekistan = response.body[2];
-        expect(uzbekistan.name.common).to.equal("Uzbekistan");
-
-        const kazakhstan = response.body[3];
-        expect(kazakhstan.name.common).to.equal("Kazakhstan");
-
-        const turkmenistan = response.body[4];
-        expect(turkmenistan.name.common).to.equal("Turkmenistan");
-
-        const belarus = response.body[5];
-        expect(belarus.name.common).to.equal("Belarus");
-
-        const tajikistan = response.body[6];
-        expect(tajikistan.name.common).to.equal("Tajikistan");
-
-        cy.log(JSON.stringify(kyrgyzstan.name.common));
-        cy.log(JSON.stringify(kyrgyzstan.languages));
-        cy.log(JSON.stringify(russia.name.common));
-        cy.log(JSON.stringify(russia.languages));
-        cy.log(JSON.stringify(uzbekistan.name.common));
-        cy.log(JSON.stringify(uzbekistan.languages));
-        cy.log(JSON.stringify(kazakhstan.name.common));
-        cy.log(JSON.stringify(kazakhstan.languages));
-        cy.log(JSON.stringify(turkmenistan.name.common));
-        cy.log(JSON.stringify(turkmenistan.languages));
-        cy.log(JSON.stringify(belarus.name.common));
-        cy.log(JSON.stringify(belarus.languages));
-        cy.log(JSON.stringify(tajikistan.name.common));
-        cy.log(JSON.stringify(tajikistan.languages));
+          cy.log(`Common Name ${index + 1}: ${commonName}`);
+          cy.log(`language ${index + 1}:`);
+          cy.log(JSON.stringify(language));
+        });
       }
     );
   });
 
-  it("3- Test the property 'languages' by showing that the endpoint, which is the language's name, must have to be perfect match. We removed one letter and it wil give a error response of 404", () => {
+  it("3- Test the property 'languages' by showing that the endpoint performs a perfect match against the country's languages. We removed one letter and it will give an error response of 404", () => {
     cy.request({
       method: "GET",
       url: "https://restcountries.com/v3.1/lang/rUssIa",
@@ -137,7 +88,7 @@ describe("API test for endpoint GET /lang/{language}", () => {
     cy.wait(1000);
   });
 
-  it("4- Test the property 'languages' by showing that the endpoint, which is the language's abbreviation, is case-insensitive", () => {
+  it("4- Test the property 'languages' by showing that the endpoint, which is a language's abbreviation, performs a case-insensitive match against the country's language abbreviation", () => {
     cy.request("GET", "https://restcountries.com/v3.1/lang/hyE").then(
       (response) => {
         // Assertion 1: Response is 200
@@ -145,8 +96,10 @@ describe("API test for endpoint GET /lang/{language}", () => {
 
         // Assertion 2: The array contains only one object (one country)
         cy.wrap(response.body).should("have.length", 1);
+        cy.log("There is only 1 country");
 
         /*
+        Assertion 3:
         This one country is Armenia ("languages": {
             "hye": "Armenian"
         })
@@ -177,7 +130,6 @@ describe("API test for endpoint GET /lang/{language}", () => {
         message: "Not Found",
       });
     });
-    cy.wait(1000);
   });
 
   it("6- Test the property 'languages' by showing that the endpoint, which is the language's abbreviation, can give more than one country", () => {
@@ -188,6 +140,7 @@ describe("API test for endpoint GET /lang/{language}", () => {
 
         // Assertion 2: The array contains 7 objects (Seven countries)
         cy.wrap(response.body).should("have.length", 7);
+        cy.log("There are 7 countries");
 
         /*
                Assertion 3:
@@ -200,46 +153,20 @@ describe("API test for endpoint GET /lang/{language}", () => {
                    - Kyrgyzstan
                    - Belarus
            */
-        const kyrgyzstan = response.body[0];
-        expect(kyrgyzstan.name.common).to.equal("Kyrgyzstan");
+        response.body.forEach((country, index) => {
+          // Access the different properties
+          const commonName = country.name.common;
+          const language = country.languages;
 
-        const russia = response.body[1];
-        expect(russia.name.common).to.equal("Russia");
-
-        const uzbekistan = response.body[2];
-        expect(uzbekistan.name.common).to.equal("Uzbekistan");
-
-        const kazakhstan = response.body[3];
-        expect(kazakhstan.name.common).to.equal("Kazakhstan");
-
-        const turkmenistan = response.body[4];
-        expect(turkmenistan.name.common).to.equal("Turkmenistan");
-
-        const belarus = response.body[5];
-        expect(belarus.name.common).to.equal("Belarus");
-
-        const tajikistan = response.body[6];
-        expect(tajikistan.name.common).to.equal("Tajikistan");
-
-        cy.log(JSON.stringify(kyrgyzstan.name.common));
-        cy.log(JSON.stringify(kyrgyzstan.languages));
-        cy.log(JSON.stringify(russia.name.common));
-        cy.log(JSON.stringify(russia.languages));
-        cy.log(JSON.stringify(uzbekistan.name.common));
-        cy.log(JSON.stringify(uzbekistan.languages));
-        cy.log(JSON.stringify(kazakhstan.name.common));
-        cy.log(JSON.stringify(kazakhstan.languages));
-        cy.log(JSON.stringify(turkmenistan.name.common));
-        cy.log(JSON.stringify(turkmenistan.languages));
-        cy.log(JSON.stringify(belarus.name.common));
-        cy.log(JSON.stringify(belarus.languages));
-        cy.log(JSON.stringify(tajikistan.name.common));
-        cy.log(JSON.stringify(tajikistan.languages));
+          cy.log(`Common Name ${index + 1}: ${commonName}`);
+          cy.log(`language ${index + 1}:`);
+          cy.log(JSON.stringify(language));
+        });
       }
     );
   });
 
-  it("7- Test the query parameter 'fields' and get 1 property 'translations'", () => {
+  it("7- Test the query parameter 'fields' and get 2 properties 'name' and 'cca2' ", () => {
     cy.request(
       "GET",
       "https://restcountries.com/v3.1/lang/Russian?fields=name,cca2"
@@ -249,6 +176,7 @@ describe("API test for endpoint GET /lang/{language}", () => {
 
       // Assertion 2: The array contains 7 objects (Seven countries)
       cy.wrap(response.body).should("have.length", 7);
+      cy.log("There are 7 countries");
 
       /*
         The countries are:
@@ -260,85 +188,26 @@ describe("API test for endpoint GET /lang/{language}", () => {
             - Kyrgyzstan
             - Belarus
         */
-      const kyrgyzstan = response.body[0];
-      expect(kyrgyzstan.name.common).to.equal("Kyrgyzstan");
-      expect(kyrgyzstan.name.official).to.equal("Kyrgyz Republic");
-      expect(kyrgyzstan.cca2).to.equal("KG");
+      response.body.forEach((country, index) => {
+        // Access the different properties
+        const commonName = country.name.common;
 
-      const russia = response.body[1];
-      expect(russia.name.common).to.equal("Russia");
-      expect(russia.name.official).to.equal("Russian Federation");
-      expect(russia.cca2).to.equal("RU");
-
-      const uzbekistan = response.body[2];
-      expect(uzbekistan.name.common).to.equal("Uzbekistan");
-      expect(uzbekistan.name.official).to.equal("Republic of Uzbekistan");
-      expect(uzbekistan.cca2).to.equal("UZ");
-
-      const kazakhstan = response.body[3];
-      expect(kazakhstan.name.common).to.equal("Kazakhstan");
-      expect(kazakhstan.name.official).to.equal("Republic of Kazakhstan");
-      expect(kazakhstan.cca2).to.equal("KZ");
-
-      const turkmenistan = response.body[4];
-      expect(turkmenistan.name.common).to.equal("Turkmenistan");
-      expect(turkmenistan.name.official).to.equal("Turkmenistan");
-      expect(turkmenistan.cca2).to.equal("TM");
-
-      const belarus = response.body[5];
-      expect(belarus.name.common).to.equal("Belarus");
-      expect(belarus.name.official).to.equal("Republic of Belarus");
-      expect(belarus.cca2).to.equal("BY");
-
-      const tajikistan = response.body[6];
-      expect(tajikistan.name.common).to.equal("Tajikistan");
-      expect(tajikistan.name.official).to.equal("Republic of Tajikistan");
-      expect(tajikistan.cca2).to.equal("TJ");
-
-      cy.log(JSON.stringify(kyrgyzstan.name.common));
-      cy.log(JSON.stringify(kyrgyzstan.cca2));
-      cy.log(JSON.stringify(russia.name.common));
-      cy.log(JSON.stringify(russia.cca2));
-      cy.log(JSON.stringify(uzbekistan.name.common));
-      cy.log(JSON.stringify(uzbekistan.cca2));
-      cy.log(JSON.stringify(kazakhstan.name.common));
-      cy.log(JSON.stringify(kazakhstan.cca2));
-      cy.log(JSON.stringify(turkmenistan.name.common));
-      cy.log(JSON.stringify(turkmenistan.cca2));
-      cy.log(JSON.stringify(belarus.name.common));
-      cy.log(JSON.stringify(belarus.cca2));
-      cy.log(JSON.stringify(tajikistan.name.common));
-      cy.log(JSON.stringify(tajikistan.cca2));
+        cy.log(`Common Name ${index + 1}: ${commonName}`);
+      });
 
       //   Only 2 properties name and cca2 related to the 7 countries is present
       const expectedProperties = ["name", "cca2"];
 
-      expectedProperties.forEach((property) => {
-        expect(kyrgyzstan).to.have.property(property);
-      });
-
-      expectedProperties.forEach((property) => {
-        expect(russia).to.have.property(property);
-      });
-
-      expectedProperties.forEach((property) => {
-        expect(uzbekistan).to.have.property(property);
-      });
-
-      expectedProperties.forEach((property) => {
-        expect(kazakhstan).to.have.property(property);
-      });
-
-      expectedProperties.forEach((property) => {
-        expect(turkmenistan).to.have.property(property);
-      });
-
-      expectedProperties.forEach((property) => {
-        expect(belarus).to.have.property(property);
-      });
-
-      expectedProperties.forEach((property) => {
-        expect(tajikistan).to.have.property(property);
+      // This code is using the forEach method to iterate over each element in the response.body array. In this case, each element represents a country in the JSON response. The (country, index) parameters in the callback function represent the current country object and its index in the array
+      response.body.forEach((country, index) => {
+        // Check if each expected property exists for the current country
+        // Within the outer loop, there's another loop using forEach, this time iterating over the expectedProperties array. This array contains the names of properties that are expected to exist for each country (in this case, "name" and "capital")
+        expectedProperties.forEach((property) => {
+          // This line uses the Chai assertion library, which is commonly used with Cypress. It checks whether the country object has the specified property (property). If the property exists, the test continues; otherwise, it fails
+          expect(country).to.have.property(property);
+          // This line logs a message using cy.log(). It indicates that the specific property (property) exists for the current country (Country ${index + 1})
+          cy.log(`Country ${index + 1}: ${property} exists`);
+        });
       });
     });
   });
