0b4d89d7595183be90f845066ea3418c774e48d3
ronyhourani <ronyhourani@gmail.com>
2023-11-21 17:13:40 +0200

    [Portf/Todolist] Solving the cross-origin problem

--- projects/qa/cypress/cypress.config.js
+++ projects/qa/cypress/cypress.config.js
@@ -16,4 +16,6 @@ module.exports = defineConfig({
     },
     specPattern: "cypress/integration/**/*.js",
   },
+  // I added this to solved the cross-origin error ( https://stackoverflow.com/questions/50495996/cypress-e2e-testing-how-to-get-around-cross-origin-errors/69987296#69987296)
+  chromeWebSecurity: false,
 });
