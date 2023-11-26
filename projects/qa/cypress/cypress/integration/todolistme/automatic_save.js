56b6e97f4274c0f5c238d117e77f8f36153a9ba7
ronyhourani <ronyhourani@gmail.com>
2023-11-26 18:54:13 +0200

    [Portf/Todolist] Change the JS name

--- projects/qa/cypress/cypress/integration/todolistme/automatic_save.js
+++ projects/qa/cypress/cypress/integration/todolistme/automatic_save.js
@@ -1,6 +1,6 @@
 /// <reference types="Cypress" />
 
-describe("Feature: Todolist page saved automatically", () => {
+describe("Feature: Automatic save", () => {
   beforeEach(() => {
     // Prereq.: Todolistme page is already opened
     cy.visit("https://todolistme.net/");
