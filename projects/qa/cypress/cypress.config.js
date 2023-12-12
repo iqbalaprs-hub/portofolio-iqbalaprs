b4d7fa2dbf2dba7ad25fda91e4e3fcd5112edb7f
ronyhourani <ronyhourani@gmail.com>
2023-12-12 18:55:30 +0200

    [Portf/Twitter] Add environment variable

--- projects/qa/cypress/cypress.config.js
+++ projects/qa/cypress/cypress.config.js
@@ -16,6 +16,11 @@ module.exports = defineConfig({
     },
     specPattern: "cypress/integration/**/*.js",
   },
+  // the env property is used to define environment variables that can be accessed within your Cypress tests.
+  // This configuration is setting an environment variable named twitterCloneBaseUrl with the value "http://localhost:3000". The purpose of this might be to specify the base URL of a Twitter clone application that your Cypress tests will interact with
+  env: {
+    twitterCloneBaseUrl: "http://localhost:3000",
+  },
   // I added this to solved the cross-origin error ( https://stackoverflow.com/questions/50495996/cypress-e2e-testing-how-to-get-around-cross-origin-errors/69987296#69987296)
   chromeWebSecurity: false,
 });
