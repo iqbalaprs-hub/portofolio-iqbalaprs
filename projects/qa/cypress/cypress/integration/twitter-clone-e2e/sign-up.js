bf6210b7c948bbc778d39a3f945cd1b5cdcf7f06
ronyhourani <ronyhourani@gmail.com>
2023-12-13 15:43:45 +0200

    [Portf/twitter] Add testcase7 in testsuite1

--- projects/qa/cypress/cypress/integration/twitter-clone-e2e/sign-up.js
+++ projects/qa/cypress/cypress/integration/twitter-clone-e2e/sign-up.js
@@ -331,4 +331,103 @@ describe("Feature: Sign Up", () => {
       .should("have.text", "email is required")
       .should("have.css", "color", "rgb(226, 61, 104)");
   });
+
+  it("7- Nominal case: The user writes a password that is not secured", () => {
+    // 7.1: The user clicks on the "Sign Up" button
+    cy.get('a[data-cy="nav-signup-link"]').click();
+
+    /*
+    7.2: 
+    The user fills the "Sign Up" form:
+      - Name: Jhonny
+      - Username: johnny
+      - email: jhonny@gmail.com
+      - Password: jhonny
+      - Confirm password: jhonny
+    */
+    cy.get('form[data-cy="signup-form"]')
+      .find('input[data-cy="signup-name-input"]')
+      .type("Jhonny");
+    cy.get('form[data-cy="signup-form"]')
+      .find('input[data-cy="signup-username-input"]')
+      .type("johnny");
+    cy.get('form[data-cy="signup-form"]')
+      .find('input[data-cy="signup-email-input"]')
+      .type("jhonny@gmail.com");
+    cy.get('form[data-cy="signup-form"]')
+      .find('input[data-cy="signup-password-input"]')
+      .type("jhonny");
+    cy.get('form[data-cy="signup-form"]')
+      .find('input[data-cy="signup-password2-input"]')
+      .type("jhonny");
+
+    // 7.3: The user clicks the "Sign Up" submit button
+    cy.get('form[data-cy="signup-form"]')
+      .find('button[data-cy="signup-submit"]')
+      .click();
+
+    // Expected result: A red sentence appears: password must be at least 8 characters
+    cy.get('h1[data-cy="signup-title"]')
+      .next("p")
+      .should("have.text", "password must be at least 8 characters")
+      .should("have.css", "color", "rgb(226, 61, 104)");
+
+    // 7.4: The user writes the password: Clonejhonny (of course with the "confirm password" being the same) and clicks the "Sign Up" submit button
+    cy.get('form[data-cy="signup-form"]')
+      .find('input[data-cy="signup-password-input"]')
+      .type("Clonejhonny");
+    cy.get('form[data-cy="signup-form"]')
+      .find('input[data-cy="signup-password2-input"]')
+      .type("Clonejhonny");
+
+    cy.get('form[data-cy="signup-form"]')
+      .find('button[data-cy="signup-submit"]')
+      .click();
+
+    // Expected result: A red sentence appears: password must contain at least 1 letter and 1 number
+    cy.get('h1[data-cy="signup-title"]')
+      .next("p")
+      .should(
+        "have.text",
+        "password must contain at least 1 letter and 1 number"
+      )
+      .should("have.css", "color", "rgb(226, 61, 104)");
+
+    // 7.5: The user writes the password: Clonejhonny23 (of course with the "confirm password" being the same) and clicks the "Sign Up" submit button
+    cy.get('form[data-cy="signup-form"]')
+      .find('input[data-cy="signup-password-input"]')
+      .type("Clonejhonny23");
+    cy.get('form[data-cy="signup-form"]')
+      .find('input[data-cy="signup-password2-input"]')
+      .type("Clonejhonny23");
+
+    cy.get('form[data-cy="signup-form"]')
+      .find('button[data-cy="signup-submit"]')
+      .click();
+
+    // Expected result: The user's account is created
+    cy.get("reach-portal")
+      .children("div")
+      .first()
+      .children("div")
+      .first()
+      .children("p")
+      .first()
+      .should("have.text", "Jhonny");
+
+    cy.get("reach-portal")
+      .children("div")
+      .first()
+      .children("div")
+      .first()
+      .children("p")
+      .eq(1)
+      .should("have.text", "@johnny");
+
+    // Expected result: The user is automatically signed in
+    cy.get('a[data-cy="nav-signin-link"]').should("not.exist");
+
+    // Expected result: The user is taken to "Home" page
+    cy.get("ul").contains("a", "Home").should("have.class", "active");
+  });
 });
