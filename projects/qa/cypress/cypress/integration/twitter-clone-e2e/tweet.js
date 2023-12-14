19be59e42189b081e19ec9baca9416d07e0bbb0c
ronyhourani <ronyhourani@gmail.com>
2023-12-14 09:30:44 +0200

    [Portf/twitter] Add testcase1 for testcase3

--- projects/qa/cypress/cypress/integration/twitter-clone-e2e/tweet.js
+++ projects/qa/cypress/cypress/integration/twitter-clone-e2e/tweet.js
@@ -35,5 +35,130 @@ describe("Feature: Tweet", () => {
       .click();
   });
 
-  it("My SecondTest case", function () {});
+  it("1- Nominal case: The user creates a tweet", () => {
+    // 1.1: The user clicks on the "Tweet" button in the navigation bar
+    cy.contains("button", "Tweet").click();
+
+    // Expected result: A tweet modal appears
+    cy.get('div[class*="DialogContent"]').should(
+      "have.attr",
+      "aria-modal",
+      "true"
+    );
+
+    // Expected result: The text box has the placeholder “What’s happening?”
+    cy.get('div[class*="DialogContent"]')
+      .find("textarea")
+      .should("have.attr", "placeholder", "What's happening?");
+
+    // Expected result: The tweet submit button is disabled since the text box is empty
+    cy.get('div[class*="DialogContent"]')
+      .find('button:contains("Tweet")')
+      .should("be.disabled");
+
+    // 1.2: The user types in the text box: "Hello everyone"
+    cy.get('div[class*="DialogContent"]')
+      .find("textarea")
+      .type("Hello everyone");
+
+    // Expected result: The tweet submit button is active since the text box is no longer empty
+    cy.get('div[class*="DialogContent"]')
+      .find('button:contains("Tweet")')
+      .should("not.be.disabled");
+
+    // 1.3: The user clicks on the tweet submit button
+    cy.get('div[class*="DialogContent"]')
+      .find('button:contains("Tweet")')
+      .click();
+
+    // Expected result: The tweet appears in the "Home" page
+    // The text
+    cy.get('div[class*="Homepage"]')
+      .find("ul")
+      .should("have.length", 1)
+      .find("li")
+      .should("have.length", 1)
+      .find("p")
+      .should("have.text", "Hello everyone");
+
+    // Name of the author
+    cy.get('div[class*="Homepage"]')
+      .find('div[class*="TweetUserGroup"]')
+      .find('span[class*="TweetUserName"]')
+      .should("have.text", "John");
+
+    // Username of the author
+    cy.get('div[class*="Homepage"]')
+      .find('div[class*="TweetUserGroup"]')
+      .find("span:nth-child(2)")
+      .should("have.text", "@john");
+
+    // The tweet date publication
+    cy.get('div[class*="Homepage"]')
+      .find('div[class*="TweetUserGroup"]')
+      .find("span:nth-child(3)")
+      .should("have.text", "14 December 2023");
+
+    // Reply button
+    cy.get('div[class*="Homepage"]')
+      .find('div[class*="TweetBottomGroup"]')
+      .find("button:nth-child(1)")
+      .should("exist")
+      .find("span")
+      .should("have.text", "0");
+
+    // Like button
+    // cy.get('div[class*="Homepage"]')
+    //   .find('div[class*="TweetBottomGroup"]')
+    //   .find("button:nth-child(2)")
+    //   .should("exist")
+    //   .should("have.text", "0");
+
+    // 1.4: The user clicks on his personal menu and opens "profile"
+    cy.get("button#menu-button--menu").click();
+    cy.get("a#option-0--menu--1").click();
+
+    // Expected result: The user sees the tweet done in step 1.2 in the tweet section
+    // The text
+    cy.get('div[class*="ProfileTweetsBoard"]')
+      .find("ul")
+      .should("have.length", 1)
+      .find("li")
+      .should("have.length", 1)
+      .find("p")
+      .should("have.text", "Hello everyone");
+
+    // Name of the author
+    cy.get('div[class*="ProfileTweetsBoard"]')
+      .find('div[class*="TweetUserGroup"]')
+      .find('span[class*="TweetUserName"]')
+      .should("have.text", "John");
+
+    // Username of the author
+    cy.get('div[class*="ProfileTweetsBoard"]')
+      .find('div[class*="TweetUserGroup"]')
+      .find("span:nth-child(2)")
+      .should("have.text", "@john");
+
+    // The tweet date publication
+    cy.get('div[class*="ProfileTweetsBoard"]')
+      .find('div[class*="TweetUserGroup"]')
+      .find("span:nth-child(3)")
+      .should("have.text", "14 December 2023");
+
+    // Reply button
+    cy.get('div[class*="ProfileTweetsBoard"]')
+      .find('div[class*="TweetBottomGroup"]')
+      .find("button:nth-child(1)")
+      .should("exist")
+      .find("span")
+      .should("have.text", "0");
+
+    // Like button
+    // cy.get('div[class*="ProfileTweetsBoard"]')
+    //   .find('div[class*="TweetBottomGroup"]')
+    //   .find("button:nth-child(2)")
+    //   .should("exist")
+    //   .should("have.text", "0");
+  });
 });
