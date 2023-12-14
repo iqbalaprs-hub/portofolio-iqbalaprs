dc8a01e184688818261c90b883fde04698b13504
ronyhourani <ronyhourani@gmail.com>
2023-12-14 08:36:52 +0200

    [Portf/twitter] Add beforeeach() for testsuite3

---
+++ projects/qa/cypress/cypress/integration/twitter-clone-e2e/tweet.js
@@ -0,0 +1,39 @@
+/// <reference types="Cypress" />
+
+describe("Feature: Tweet", () => {
+  beforeEach(() => {
+    // Prereq.: The database is empty. There are no users in the database
+    cy.exec(".\\cypress\\scripts\\twitter-clone-e2e\\clear_mongo.bat");
+
+    /*
+        Import the data in "twitter clone test data/3- TWEET" in mongoDb
+        The data contains the users:
+            1- Name: John (username: john; email: john@gmail.com; password: Clonejohn23)
+            2- Name: Paul (username: paul; email: paul@gmail.com; password: Clonepaul23)
+            3- Name: Rony (username: rony; email: rony@gmail.com; password: Clonerony23)
+    */
+    cy.exec(
+      ".\\cypress\\scripts\\twitter-clone-e2e\\import_data_to_mongo.bat users .\\cypress\\fixtures\\twitter-clone-e2e\\tweet\\twitter-clone-db.users.json"
+    );
+    cy.exec(
+      ".\\cypress\\scripts\\twitter-clone-e2e\\import_data_to_mongo.bat profiles .\\cypress\\fixtures\\twitter-clone-e2e\\tweet\\twitter-clone-db.profiles.json"
+    );
+
+    // Prereq.: Open Twitter-Clone as an anonymous user
+    cy.visit(Cypress.env("twitterCloneBaseUrl"));
+
+    // Prereq.: The user sign in as John
+    cy.get('a[data-cy="nav-signin-link"]').click();
+    cy.get('form[data-cy="signin-form"]')
+      .find('input[data-cy="signin-username-input"]')
+      .type("john");
+    cy.get('form[data-cy="signin-form"]')
+      .find('input[data-cy="signin-password-input"]')
+      .type("Clonejohn23");
+    cy.get('form[data-cy="signin-form"]')
+      .find('button[data-cy="signin-button"]')
+      .click();
+  });
+
+  it("My SecondTest case", function () {});
+});
