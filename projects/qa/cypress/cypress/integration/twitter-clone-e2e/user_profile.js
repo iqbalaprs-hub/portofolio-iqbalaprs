ae465b2569b3c65129993a34afd168a08684ea5a
ronyhourani <ronyhourani@gmail.com>
2023-12-17 22:32:42 +0200

    [Portf/twitter] Add testcases 4&5 for testsuite8

--- projects/qa/cypress/cypress/integration/twitter-clone-e2e/user_profile.js
+++ projects/qa/cypress/cypress/integration/twitter-clone-e2e/user_profile.js
@@ -18,7 +18,7 @@ describe("Feature: User's profile", () => {
                 3- I am going to work now
                 4- Meet me all at the office
                 5- We finish work early today at noon
-        */
+    */
     cy.exec(
       ".\\cypress\\scripts\\twitter-clone-e2e\\import_data_to_mongo.bat users .\\cypress\\fixtures\\twitter-clone-e2e\\users_profile\\twitter-clone-db.users.json"
     );
@@ -31,16 +31,7 @@ describe("Feature: User's profile", () => {
 
     // Prereq.: Open Twitter-Clone and sign in as John
     cy.visit(Cypress.env("twitterCloneBaseUrl"));
-    cy.get('a[data-cy="nav-signin-link"]').click();
-    cy.get('form[data-cy="signin-form"]')
-      .find('input[data-cy="signin-username-input"]')
-      .type("john");
-    cy.get('form[data-cy="signin-form"]')
-      .find('input[data-cy="signin-password-input"]')
-      .type("Clonejohn23");
-    cy.get('form[data-cy="signin-form"]')
-      .find('button[data-cy="signin-button"]')
-      .click();
+    cy.SignInAsJohnInTwitterClone();
   });
 
   it("1- Nominal case: The user enters a profile", () => {
@@ -140,7 +131,7 @@ describe("Feature: User's profile", () => {
       .should("exist");
   });
 
-  it.only("3- Nominal case: The user can see the tweets in the tweet section of the user's profile", () => {
+  it("3- Nominal case: The user can see the tweets in the tweet section of the user's profile", () => {
     // 3.1: The user  clicks on "All profiles" button in the navigation bar
     cy.get('nav[class*="MainNav"]').contains("a", "All profiles").click();
 
@@ -156,6 +147,11 @@ describe("Feature: User's profile", () => {
         The user sees 5 tweets in the tweet section
 
         The tweets are sorted from newest (Top) to oldest (Bottom)
+          - We finish work early today at noon
+          - Meet me all at the office
+          - I am going to work now
+          - I hope you are all doing well
+          - Good morning everyone
     */
     cy.get('div[class*="ProfileTweetsBoard"]')
       .find("ul")
@@ -212,5 +208,190 @@ describe("Feature: User's profile", () => {
     cy.get('div[class*="ProfileTweetsBoard"]')
       .find("ul p")
       .should("have.text", "There are no tweets to display");
+  });
+
+  it("4- Nominal case: The user can see the likes in the like section of the user's profile", () => {
+    // 4.1: The user  clicks on "All profiles" button in the navigation bar
+    cy.get('nav[class*="MainNav"]').contains("a", "All profiles").click();
+
+    // 4.2: The user  clicks on John's profile (John @john)
+    cy.get('ul[class*="ProfilesList"]')
+      .find("li:nth-child(1)")
+      .find('div[class*="TextContainer"]')
+      .find("p:nth-child(2)")
+      .should("have.text", "@john")
+      .click();
+
+    // 4.3: The user enters the likes section
+    cy.get('div[class*="ProfileHeaderMenu"]')
+      .find('ul[class*="HeaderMenuList"] a:nth-child(2)')
+      .click();
+
+    /*
+    There are no tweets displayed since John didn't like any tweet
+    There is the sentence: There are no tweets to display
+    */
+    cy.get('div[class*="ProfileTweetsBoard"]')
+      .find("ul")
+      .find("li")
+      .should("not.exist");
+
+    cy.get('div[class*="ProfileTweetsBoard"]')
+      .find("ul p")
+      .should("have.text", "There are no tweets to display");
+
+    // 4.4: The user John signs out
+    cy.get('button[data-cy="auth-nav-dropdown-button"]').click();
+    cy.get('div[data-cy="auth-nav-logout-button"]').click();
+
+    // 4.5: The user Rony Signs In
+    cy.SignInAsRonyInTwitterClone();
+
+    /*
+    4.6:
+    The user  Rony clicks "Home" in the navigation bar and like the following tweets:
+      - I hope you are all doing well
+      - We finish work early today at noon
+    */
+    cy.get("ul").contains("a", "Home").click();
+    cy.get('div[class*="Homepage"]')
+      .find("ul li:nth-child(4)")
+      .find('div[class*="TweetBottomGroup"]')
+      .find('span[class*="LikeIcon"]')
+      .click();
+    cy.get('div[class*="Homepage"]')
+      .find("ul li:nth-child(1)")
+      .find('div[class*="TweetBottomGroup"]')
+      .find('span[class*="LikeIcon"]')
+      .click();
+
+    // 4.7: The user Rony clicks on "All profiles" button in the navigation bar
+    cy.get('nav[class*="MainNav"]').contains("a", "All profiles").click();
+
+    // 4.8: The user  clicks on Rony's profile (Rony @rony) and enter the likes section
+    cy.get('ul[class*="ProfilesList"]')
+      .find("li:nth-child(2)")
+      .find('div[class*="TextContainer"]')
+      .find("p:nth-child(2)")
+      .should("have.text", "@rony")
+      .click();
+    cy.get('div[class*="ProfileHeaderMenu"]')
+      .find('ul[class*="HeaderMenuList"] a:nth-child(2)')
+      .click();
+
+    // Expected result: The 2 tweets liked apppears from newest (Top) to oldest (Bottom)
+    cy.get('div[class*="ProfileTweetsBoard"]')
+      .find("ul li:nth-child(1)")
+      .find('div[class*="TweetBottomGroup"]')
+      .find("button:nth-child(2)")
+      .should("have.text", " 1");
+
+    cy.get('div[class*="ProfileTweetsBoard"]')
+      .find("ul li:nth-child(1)")
+      .find('div[class*="TweetBottomGroup"]')
+      .find('button:nth-child(2) span[class*="LikeIcon"]')
+      .should("have.css", "color", "rgb(226, 61, 104)");
+
+    cy.get('div[class*="ProfileTweetsBoard"]')
+      .find("ul li:nth-child(2)")
+      .find('div[class*="TweetBottomGroup"]')
+      .find("button:nth-child(2)")
+      .should("have.text", " 1");
+
+    cy.get('div[class*="ProfileTweetsBoard"]')
+      .find("ul li:nth-child(2)")
+      .find('div[class*="TweetBottomGroup"]')
+      .find('button:nth-child(2) span[class*="LikeIcon"]')
+      .should("have.css", "color", "rgb(226, 61, 104)");
+  });
+
+  it("5- Edge case: The tweets in the likes section are sorted depending on when they were created, not when they were liked", () => {
+    // 5.1: The user clicks "Home" in the navigation bar
+    cy.get("ul").contains("a", "Home").click();
+
+    // 5.2: The user likes all his 5 tweets form oldest (bottom) to newest (top)
+    cy.get('div[class*="Homepage"]')
+      .find("ul li:nth-child(5)")
+      .find('div[class*="TweetBottomGroup"]')
+      .find('span[class*="LikeIcon"]')
+      .click();
+    cy.get('div[class*="Homepage"]')
+      .find("ul li:nth-child(4)")
+      .find('div[class*="TweetBottomGroup"]')
+      .find('span[class*="LikeIcon"]')
+      .click();
+    cy.get('div[class*="Homepage"]')
+      .find("ul li:nth-child(3)")
+      .find('div[class*="TweetBottomGroup"]')
+      .find('span[class*="LikeIcon"]')
+      .click();
+    cy.get('div[class*="Homepage"]')
+      .find("ul li:nth-child(2)")
+      .find('div[class*="TweetBottomGroup"]')
+      .find('span[class*="LikeIcon"]')
+      .click();
+    cy.get('div[class*="Homepage"]')
+      .find("ul li:nth-child(1)")
+      .find('div[class*="TweetBottomGroup"]')
+      .find('span[class*="LikeIcon"]')
+      .click();
+
+    // 5.3: The user  clicks on "All profiles" button in the navigation bar
+    cy.get('nav[class*="MainNav"]').contains("a", "All profiles").click();
+
+    // 5.4: The user  clicks on John's profile (John @john)
+    cy.get('ul[class*="ProfilesList"]')
+      .find("li:nth-child(1)")
+      .find('div[class*="TextContainer"]')
+      .find("p:nth-child(2)")
+      .should("have.text", "@john")
+      .click();
+
+    // 5.5: The user enters the likes section
+    cy.get('div[class*="ProfileHeaderMenu"]')
+      .find('ul[class*="HeaderMenuList"] a:nth-child(2)')
+      .click();
+
+    /*
+    Expected result:
+    All 5 tweets are all present in the likes section from newest (Top) to oldest (Bottom)
+      - We finish work early today at noon
+      - Meet me all at the office
+      - I am going to work now
+      - I hope you are all doing well
+      - Good morning everyone
+
+    NB: The tweets are sorted depending on when they were created, NOT when they were liked
+    */
+    cy.get('div[class*="ProfileTweetsBoard"]')
+      .find("ul")
+      .should("have.length", 1)
+      .find("li")
+      .should("have.length", 5);
+
+    cy.get('div[class*="ProfileTweetsBoard"]')
+      .find("ul li:nth-child(1)")
+      .find("p")
+      .should("have.text", "We finish work early today at noon");
+
+    cy.get('div[class*="ProfileTweetsBoard"]')
+      .find("ul li:nth-child(2)")
+      .find("p")
+      .should("have.text", "Meet me all at the office");
+
+    cy.get('div[class*="ProfileTweetsBoard"]')
+      .find("ul li:nth-child(3)")
+      .find("p")
+      .should("have.text", "I am going to work now");
+
+    cy.get('div[class*="ProfileTweetsBoard"]')
+      .find("ul li:nth-child(4)")
+      .find("p")
+      .should("have.text", "I hope you are all doing well");
+
+    cy.get('div[class*="ProfileTweetsBoard"]')
+      .find("ul li:nth-child(5)")
+      .find("p")
+      .should("have.text", "Good morning everyone");
   });
 });
