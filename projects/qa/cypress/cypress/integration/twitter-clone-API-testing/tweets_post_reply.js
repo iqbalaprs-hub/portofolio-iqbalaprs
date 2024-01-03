2bb5eabf4dedf5932e880f2580c538a72a25f797
ronyhourani <ronyhourani@gmail.com>
2024-01-03 20:43:07 +0200

    [Portf/twitter]Change name&fix details for testsuite replytotweet

--- projects/qa/cypress/cypress/integration/twitter-clone-API-testing/tweets_post_reply.js
+++ projects/qa/cypress/cypress/integration/twitter-clone-API-testing/tweets_post_reply.js
@@ -1,9 +1,9 @@
 /// <reference types="Cypress" />
 
-describe("Feature: Delete a tweet", () => {
+describe("Feature: Reply to a tweet", () => {
   // Declaring the variables tweetId and JohnsToken here in order to be use in the entire test suite
-  var johnsToken;
-  var tweetId;
+  let johnToken;
+  let tweetId;
 
   beforeEach(() => {
     // Prereq.: The database is empty. There are no users in the database
@@ -12,20 +12,20 @@ describe("Feature: Delete a tweet", () => {
     /*
               Prereq.:
           
-              Import the data in "twitter clone test data/REPLY TWEET " in mongoDb
+              Import the data in "twitter clone test data/tweets_post_reply " in mongoDb
               The data contains the users:
                 1- Name: John (username: john; email: john@gmail.com; password: Clonejohn23)
 
                 2- Name: Rony (username: rony; email: rony@gmail.com; password: Clonerony23)
               */
     cy.exec(
-      ".\\cypress\\scripts\\twitter-clone-e2e\\import_data_to_mongo.bat users .\\cypress\\fixtures\\twitter-clone-API-testing\\replyTweet\\twitter-clone-db.users.json"
+      ".\\cypress\\scripts\\twitter-clone-e2e\\import_data_to_mongo.bat users .\\cypress\\fixtures\\twitter-clone-API-testing\\tweets_post_reply\\twitter-clone-db.users.json"
     );
     cy.exec(
-      ".\\cypress\\scripts\\twitter-clone-e2e\\import_data_to_mongo.bat profiles .\\cypress\\fixtures\\twitter-clone--API-testing\\replyTweet\\twitter-clone-db.profiles.json"
+      ".\\cypress\\scripts\\twitter-clone-e2e\\import_data_to_mongo.bat profiles .\\cypress\\fixtures\\twitter-clone--API-testing\\tweets_post_reply\\twitter-clone-db.profiles.json"
     );
 
-    // Sign in as John in order to get the John's token
+    // Sign in as John in order to get John's token
     cy.request({
       method: "POST",
       url: "http://localhost:3001/api/auth/login",
@@ -35,7 +35,7 @@ describe("Feature: Delete a tweet", () => {
       },
     }).then((loginResponse) => {
       expect(loginResponse.status).to.equal(200);
-      johnsToken = loginResponse.body.token;
+      johnToken = loginResponse.body.token;
 
       // Create a tweet using John's token in order to get this tweet's ID
       cy.request({
@@ -45,7 +45,7 @@ describe("Feature: Delete a tweet", () => {
           text: "Hello",
         },
         headers: {
-          Authorization: `Bearer ${johnsToken}`,
+          Authorization: `Bearer ${johnToken}`,
         },
       }).then((tweetResponse) => {
         expect(tweetResponse.status).to.equal(201);
@@ -64,25 +64,28 @@ describe("Feature: Delete a tweet", () => {
         replyTo: tweetId,
       },
       headers: {
-        Authorization: `Bearer ${johnsToken}`,
+        Authorization: `Bearer ${johnToken}`,
       },
     }).then((replyTweetResponse) => {
-      // Assertion 1: status is 201
+      // Assertion 1: Status is 201
       expect(replyTweetResponse.status).to.equal(201);
 
-      // Assertion 2: content of the response body is true
+      // Assertion 2: The response body contains the correct data
       expect(replyTweetResponse.body.tweet.repliesCount).to.equal(0);
       expect(replyTweetResponse.body.tweet.likes).to.deep.equal([]);
       expect(replyTweetResponse.body.tweet.retweets).to.deep.equal([]);
       expect(replyTweetResponse.body.tweet.text).to.equal("Reply");
       expect(replyTweetResponse.body.tweet.replyTo).to.equal(tweetId);
 
-      // We are extracting the ID of the tweet's reply and we are asserting that this reply is created
+      // Assertion 3: We are extracting the ID of the tweet's reply and we are asserting that this reply is created
       var replyTweetId = replyTweetResponse.body.tweet._id;
       cy.request({
         method: "GET",
         url: `http://localhost:3001/api/tweets/${replyTweetId}`,
       }).then((getTweetResponse) => {
+        // Status is 200
+        expect(getTweetResponse.status).to.equal(200);
+        // The response body contains the correct data
         expect(getTweetResponse.body.tweet.text).to.equal("Reply");
         expect(getTweetResponse.body.tweet._id).to.equal(replyTweetId);
       });
@@ -99,15 +102,58 @@ describe("Feature: Delete a tweet", () => {
         replyTo: tweetId,
       },
       headers: {
-        Authorization: `Bearer ${johnsToken}`,
+        Authorization: `Bearer ${johnToken}`,
       },
       failOnStatusCode: false, // Allows the test to continue even if the status code is not 2xx
     }).then((replyTweetResponse) => {
-      // Assertion 1: status is 400
+      // Assertion 1: Status is 400
       expect(replyTweetResponse.status).to.equal(400);
-      // Assertion 2: error message is ""text" length must be less than or equal to 280 characters long"
+      // Assertion 2: Error message is ""text" length must be less than or equal to 280 characters long"
       expect(replyTweetResponse.body.message).to.equal(
         '"text" length must be less than or equal to 280 characters long'
+      );
+    });
+  });
+
+  it("3- POST /tweets cannot create a reply with a wrong tweet ID", () => {
+    // We are creating a wrong tweet ID for testing
+    const wrongTweetId = "12345";
+    cy.request({
+      method: "POST",
+      url: "http://localhost:3001/api/tweets",
+      body: {
+        text: "Reply",
+        replyTo: wrongTweetId,
+      },
+      headers: {
+        Authorization: `Bearer ${johnToken}`,
+      },
+      failOnStatusCode: false, // Allows the test to continue even if the status code is not 2xx
+    }).then((replyTweetResponse) => {
+      // Assertion 1: Status is 400
+      expect(replyTweetResponse.status).to.equal(400);
+      // Assertion 2: Error message is "\"replyTo\" must be a valid mongo id"
+      expect(replyTweetResponse.body.message).to.equal(
+        `\"replyTo\" must be a valid mongo id`
+      );
+    });
+  });
+
+  it("4- POST /tweets cannot create a reply without a user's token", () => {
+    cy.request({
+      method: "POST",
+      url: "http://localhost:3001/api/tweets",
+      body: {
+        text: "Reply",
+        replyTo: tweetId,
+      },
+      failOnStatusCode: false, // Allows the test to continue even if the status code is not 2xx
+    }).then((replyTweetResponse) => {
+      // Assertion 1: Status is 401
+      expect(replyTweetResponse.status).to.equal(401);
+      // Assertion 2: Error message is "You are not authenticated"
+      expect(replyTweetResponse.body.message).to.equal(
+        "You are not authenticated"
       );
     });
   });
