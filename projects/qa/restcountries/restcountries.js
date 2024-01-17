ccb2c0f016fa84b356a6d979e1b5ba6e2cecae6f
ronyhourani <ronyhourani@gmail.com>
2024-01-17 09:12:51 +0200

    [Home] add all common javascripts into one common file

--- projects/qa/restcountries/restcountries.js
+++
@@ -1,8 +0,0 @@
-// Accordion behavior
-
-const accordionItemList = document.getElementsByClassName("accordion-item");
-for (const accordionItem of accordionItemList) {
-  accordionItem.addEventListener("click", function () {
-    accordionItem.classList.toggle("open");
-  });
-}
--- projects/qa/twitter-clone-performance-testing/twitter-clone-performance.js
+++ projects/qa/twitter-clone-performance-testing/twitter-clone-performance.js
@@ -1,12 +1,3 @@
-// Accordion behavior
-
-const accordionItemList = document.getElementsByClassName("accordion-item");
-for (const accordionItem of accordionItemList) {
-  accordionItem.addEventListener("click", function () {
-    accordionItem.classList.toggle("open");
-  });
-}
-
 // Canvas
 
 const canvas = document.getElementById("graph-response-time-per-user-number");
