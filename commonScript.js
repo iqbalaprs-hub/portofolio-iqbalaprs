fd63545fda832c87dc9d7a8afca676c450174e69
ronyhourani <ronyhourani@gmail.com>
2024-02-03 22:49:04 +0200

    [Home] complete project details code & organize code

--- commonScript.js
+++ commonScript.js
@@ -1,9 +1,9 @@
 // Accordion behavior
 
-const accordionItemList = document.getElementsByClassName("accordion-item");
-for (const accordionItem of accordionItemList) {
-  const accordionTitle = accordionItem.querySelector(".accordion-title");
-  accordionTitle.addEventListener("click", function () {
-    accordionItem.classList.toggle("open");
-  });
-}
+// const accordionItemList = document.getElementsByClassName("accordion-item");
+// for (const accordionItem of accordionItemList) {
+//   const accordionTitle = accordionItem.querySelector(".accordion-title");
+//   accordionTitle.addEventListener("click", function () {
+//     accordionItem.classList.toggle("open");
+//   });
+// }
