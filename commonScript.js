4faff718636a43eac6719093fd6b2149ffc64c86
ronyhourani <ronyhourani@gmail.com>
2024-02-10 22:56:59 +0200

    [Home] change class name of table of contents links in Script &  all projects

--- commonScript.js
+++ commonScript.js
@@ -18,7 +18,7 @@ for (const accordionItem of accordionItemList) {
 // Purpose of the code below: when a link in the table of contents is clicked, the script prevents the default behavior, calculates the position of the target element, and smoothly scrolls the window to the position of the target element on the screen.
 document.addEventListener("DOMContentLoaded", function () {
   // This selects all elements with the class table-of-contents-section-title, which are the links in the table of contents
-  const links = document.querySelectorAll(".table-of-contents-section-title");
+  const links = document.querySelectorAll(".reach-section-title");
 
   // Iterates through each link
   links.forEach((link) => {
@@ -37,7 +37,7 @@ document.addEventListener("DOMContentLoaded", function () {
         // Calculates the offset from the top of the document to the target element
         const offsetTop = targetElement.offsetTop;
         // Adjusted for a 115px offset from the top
-        const scrollPosition = offsetTop - 115;
+        const scrollPosition = offsetTop - 110;
 
         // Scrolls the window to the calculated position with a smooth scrolling behavior
         window.scrollTo({
